4325768d7d1579305ea08409b9676885813696ca
Matthew Leibowitz <mattleibow@live.com>
2016-04-18 19:09:19 +0200

    Updates to the way we add to native skia

--- native-builds/src/sk_xamarin.cpp
+++ native-builds/src/sk_xamarin.cpp
@@ -1,5 +1,13 @@
+/*
+ * Copyright 2016 Xamarin Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
 
 #include "sk_xamarin.h"
+
+#include "sk_types_priv.h"
 
 #include "../images/SkForceLinking.h"
 
