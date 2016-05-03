723e32f08c83d77776e15ddfd555b1ab9df12824
Charlenni <info@wfplayer.com>
2016-05-03 17:37:09 +0200

    Changed whitespace handling

--- binding/Binding/SKPath.cs
+++ binding/Binding/SKPath.cs
@@ -103,16 +103,16 @@ namespace SkiaSharp
 		}
 
 		public void Rewind()
-        {
-            SkiaApi.sk_path_rewind(Handle);
-        }
+		{
+			SkiaApi.sk_path_rewind(Handle);
+		}
 
-        public void Reset()
-        {
-            SkiaApi.sk_path_reset(Handle);
-        }
+		public void Reset()
+		{
+			SkiaApi.sk_path_reset(Handle);
+		}
 
-        public void AddRect (SKRect rect, SKPathDirection direction)
+		public void AddRect (SKRect rect, SKPathDirection direction)
 		{
 			SkiaApi.sk_path_add_rect (Handle, ref rect, direction);
 		}
