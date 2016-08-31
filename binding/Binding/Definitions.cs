26677538392b7e59e37b1cbe9ae9ed259b029bd4
Matthew Leibowitz <mattleibow@live.com>
2016-08-31 05:00:21 +0200

    Updated the font style weights to match the native enum

--- binding/Binding/Definitions.cs
+++ binding/Binding/Definitions.cs
@@ -218,6 +218,7 @@ namespace SkiaSharp
 	}
 
 	public enum SKFontStyleWeight {
+		Invisible   =   0,
 		Thin        = 100,
 		ExtraLight  = 200,
 		Light       = 300,
@@ -226,7 +227,8 @@ namespace SkiaSharp
 		SemiBold    = 600,
 		Bold        = 700,
 		ExtraBold   = 800,
-		Black       = 900
+		Black       = 900,
+		ExtraBlack  =1000,
 	};
 
 	public enum SKFontStyleWidth {
