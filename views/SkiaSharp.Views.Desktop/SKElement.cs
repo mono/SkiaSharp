a7439fc670fddb6ee22c450f8b6dd4d35cbff062
Matthew Leibowitz <mattleibow@live.com>
2016-09-09 21:56:40 +0200

    Added a WPF control

---
+++ views/SkiaSharp.Views.Desktop/SKElement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SkiaSharp.Views
+{
+	public class SKElement : FrameworkElement
+	{
+		private readonly bool designMode;
+
+		private WriteableBitmap bitmap;
+
+		public SKElement()
+		{
+			designMode = DesignerProperties.GetIsInDesignMode(this);
+		}
+
+		public event EventHandler<SKPaintSurfaceEventArgs> PaintSurface;
+
+		protected override void OnRender(DrawingContext drawingContext)
+		{
+			if (designMode)
+				return;
+
+			base.OnRender(drawingContext);
+
+			var m = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
+			var dpiX = m.M11;
+			var dpiY = m.M22;
+			var width = (int)(ActualWidth * dpiX);
+			var height = (int)(ActualHeight * dpiY);
+
+			var info = new SKImageInfo(width, height, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
+
+			// reset the bitmap if the size has changed
+			if (bitmap == null || info.Width != bitmap.PixelWidth || info.Height != bitmap.PixelHeight)
+			{
+				bitmap = new WriteableBitmap(width, height, dpiX, dpiY, PixelFormats.Pbgra32, null);
+			}
+
+			// draw on the bitmap
+			bitmap.Lock();
+			using (var surface = SKSurface.Create(info, bitmap.BackBuffer, bitmap.BackBufferStride))
+			{
+				OnPaintSurface(new SKPaintSurfaceEventArgs(surface, info));
+			}
+
+			// draw the bitmap to the screen
+			bitmap.AddDirtyRect(new Int32Rect(0, 0, width, height));
+			bitmap.Unlock();
+			drawingContext.DrawImage(bitmap, new Rect(0, 0, ActualWidth, ActualHeight));
+		}
+
+		protected virtual void OnPaintSurface(SKPaintSurfaceEventArgs e)
+		{
+			// invoke the event
+			PaintSurface?.Invoke(this, e);
+		}
+
+		protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+		{
+			base.OnRenderSizeChanged(sizeInfo);
+
+			InvalidateVisual();
+		}
+	}
+}
