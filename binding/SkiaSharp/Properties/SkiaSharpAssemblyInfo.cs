ff3ad1a534ca23fd223b1e88abde5a6c6c917719
Matthew Leibowitz <mattleibow@live.com>
2016-09-15 19:15:12 +0200

    We don't want to up the AssemblyVersion

--- binding/SkiaSharp/Properties/SkiaSharpAssemblyInfo.cs
+++ binding/SkiaSharp/Properties/SkiaSharpAssemblyInfo.cs
@@ -16,7 +16,7 @@
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.54.0.1")]
+[assembly: AssemblyVersion ("1.54.0.0")]
 [assembly: AssemblyFileVersion ("1.54.0.1")]
 [assembly: AssemblyInformationalVersion ("1.54.0.1-{GIT_SHA}")]
 
