3d2ef0b5ccf3ed9541abc92417490b3991834050
Matthew Leibowitz <mattleibow@live.com>
2016-09-16 00:52:41 +0200

    Made sure to use tabs everywhere

--- samples/Skia.Forms.Demo/Skia.Forms.Demo.cs
+++ samples/Skia.Forms.Demo/Skia.Forms.Demo.cs
@@ -8,63 +8,63 @@ using SkiaSharp.Views;
 
 namespace Skia.Forms.Demo
 {
-    public class App : Application
-    {
-        public static readonly Color XamarinBlue = Color.FromHex("3498db");
-        public static readonly Color XamarinGreen = Color.FromHex("77d065");
-        public static readonly Color XamarinPurple = Color.FromHex("b455b6");
-        public static readonly Color XamarinDark = Color.FromHex("2c3e50");
+	public class App : Application
+	{
+		public static readonly Color XamarinBlue = Color.FromHex("3498db");
+		public static readonly Color XamarinGreen = Color.FromHex("77d065");
+		public static readonly Color XamarinPurple = Color.FromHex("b455b6");
+		public static readonly Color XamarinDark = Color.FromHex("2c3e50");
 
-        public App()
-        {
-            var items =
-                Device.OS == TargetPlatform.iOS ? Demos.SamplesForPlatform(Demos.Platform.iOS) :
-                Device.OS == TargetPlatform.Android ? Demos.SamplesForPlatform(Demos.Platform.Android) :
-                Device.OS == TargetPlatform.Windows ? Demos.SamplesForPlatform(Demos.Platform.UWP) :
-                Demos.SamplesForPlatform(Demos.Platform.All);
+		public App()
+		{
+			var items =
+				Device.OS == TargetPlatform.iOS ? Demos.SamplesForPlatform(Demos.Platform.iOS) :
+				Device.OS == TargetPlatform.Android ? Demos.SamplesForPlatform(Demos.Platform.Android) :
+				Device.OS == TargetPlatform.Windows ? Demos.SamplesForPlatform(Demos.Platform.UWP) :
+				Demos.SamplesForPlatform(Demos.Platform.All);
 
-            var masterDetail = new MasterDetailPage
-            {
-                IsGestureEnabled = true,
-                MasterBehavior = MasterBehavior.Popover
-            };
-            var detailPage = new DetailsPage(items.First());
-            var navPage = new NavigationPage(detailPage)
-            {
-                BarBackgroundColor = XamarinBlue,
-                BarTextColor = Color.White,
-            };
-            var masterPage = new MasterPage(items, demo =>
-            {
-                masterDetail.IsPresented = false;
-                detailPage.SetDemo(demo);
-                navPage.Title = demo;
-            });
-            if (Device.OS == TargetPlatform.iOS)
-            {
-                masterPage.Icon = "hamburger.png";
-            }
+			var masterDetail = new MasterDetailPage
+			{
+				IsGestureEnabled = true,
+				MasterBehavior = MasterBehavior.Popover
+			};
+			var detailPage = new DetailsPage(items.First());
+			var navPage = new NavigationPage(detailPage)
+			{
+				BarBackgroundColor = XamarinBlue,
+				BarTextColor = Color.White,
+			};
+			var masterPage = new MasterPage(items, demo =>
+			{
+				masterDetail.IsPresented = false;
+				detailPage.SetDemo(demo);
+				navPage.Title = demo;
+			});
+			if (Device.OS == TargetPlatform.iOS)
+			{
+				masterPage.Icon = "hamburger.png";
+			}
 
-            masterDetail.Master = masterPage;
-            masterDetail.Detail = navPage;
+			masterDetail.Master = masterPage;
+			masterDetail.Detail = navPage;
 
-            MainPage = masterDetail;
+			MainPage = masterDetail;
 
-            masterDetail.IsPresented = true;
-        }
+			masterDetail.IsPresented = true;
+		}
 
-        public class DetailsPage : ContentPage
-        {
+		public class DetailsPage : ContentPage
+		{
 			private Demos.Sample sample;
 
-            public DetailsPage(string demo)
-            {
-                SetDemo(demo);
-            }
+			public DetailsPage(string demo)
+			{
+				SetDemo(demo);
+			}
 
-            public void SetDemo(string demo)
-            {
-                Title = demo;
+			public void SetDemo(string demo)
+			{
+				Title = demo;
 				sample = Demos.GetSample(demo);
 
 				var tap = new TapGestureRecognizer();
@@ -85,79 +85,79 @@ namespace Skia.Forms.Demo
 			private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
 			{
 				sample?.Method?.Invoke(e.Surface.Canvas, e.Info.Width, e.Info.Height);
-            }
-        }
+			}
+		}
 
-        public class MasterPage : ContentPage
-        {
-            private readonly ListView listView;
-            private readonly Action<string> showDemo;
+		public class MasterPage : ContentPage
+		{
+			private readonly ListView listView;
+			private readonly Action<string> showDemo;
 
-            public MasterPage(string[] items, Action<string> showDemo)
-            {
-                this.showDemo = showDemo;
+			public MasterPage(string[] items, Action<string> showDemo)
+			{
+				this.showDemo = showDemo;
 
-                Title = "SkiaSharp";
-                BackgroundColor = Color.FromHex("EEEEEE");
+				Title = "SkiaSharp";
+				BackgroundColor = Color.FromHex("EEEEEE");
 
-                // a nice header
-                var header = new ContentView
-                {
-                    BackgroundColor = XamarinDark,
-                    Padding = new Thickness(12, 72, 12, 12),
-                    Margin = 0,
-                    Content = new Label
-                    {
-                        Text = Title,
-                        FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                        TextColor = Color.White
-                    },
-                };
+				// a nice header
+				var header = new ContentView
+				{
+					BackgroundColor = XamarinDark,
+					Padding = new Thickness(12, 72, 12, 12),
+					Margin = 0,
+					Content = new Label
+					{
+						Text = Title,
+						FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+						TextColor = Color.White
+					},
+				};
 
-                var spacer = new BoxView
-                {
-                    Color = XamarinBlue,
-                    HorizontalOptions = LayoutOptions.Fill,
-                    Margin = 0,
-                    HeightRequest = 3
-                };
+				var spacer = new BoxView
+				{
+					Color = XamarinBlue,
+					HorizontalOptions = LayoutOptions.Fill,
+					Margin = 0,
+					HeightRequest = 3
+				};
 
-                // the contents of the menu
-                listView = new ListView()
-                {
-                    ItemsSource = items,
-                };
-                listView.ItemSelected += OnItemSelected;
+				// the contents of the menu
+				listView = new ListView()
+				{
+					ItemsSource = items,
+				};
+				listView.ItemSelected += OnItemSelected;
 
-                // UWP specific for more spacing there
-                if (Device.OS == TargetPlatform.Windows)
-                {
-                    listView.RowHeight = 30;
-                    listView.Margin = new Thickness(12, 6);
-                    header.Padding = new Thickness(12, 36, 12, 12);
-                }
+				// UWP specific for more spacing there
+				if (Device.OS == TargetPlatform.Windows)
+				{
+					listView.RowHeight = 30;
+					listView.Margin = new Thickness(12, 6);
+					header.Padding = new Thickness(12, 36, 12, 12);
+				}
 
-                // put it all together
-                Content = new StackLayout
-                {
-                    Children = { header, spacer, listView },
-                    Padding = 0,
-                    Spacing = 0
-                };
-            }
+				// put it all together
+				Content = new StackLayout
+				{
+					Children = { header, spacer, listView },
+					Padding = 0,
+					Spacing = 0
+				};
+			}
 
-            private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
-            {
-                // deselect the menu item
-                if (e.SelectedItem == null)
-                {
-                    return;
-                }
-                listView.SelectedItem = null;
+			private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+			{
+				// deselect the menu item
+				if (e.SelectedItem == null)
+				{
+					return;
+				}
+				listView.SelectedItem = null;
 
-                // display the selected demo
-                showDemo(e.SelectedItem.ToString());
-            }
-        }
-    }
+				// display the selected demo
+				showDemo(e.SelectedItem.ToString());
+			}
+		}
+	}
 }
