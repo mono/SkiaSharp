456a6488ae3adec133d65ac48692d536398a9202
Matthew Leibowitz <mattleibow@live.com>
2016-10-17 04:28:02 +0200

    Added an SVG sample

---
+++ samples/SkiaSharpSample.Shared/Samples/SvgSample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace SkiaSharpSample.Samples
+{
+	[Preserve(AllMembers = true)]
+	public class SvgSample : SampleBase
+	{
+		private SKSvg svg;
+
+		[Preserve]
+		public SvgSample()
+		{
+		}
+
+		public override string Title => "SVG";
+
+		public override SampleCategories Category => SampleCategories.BitmapDecoding | SampleCategories.SVG;
+
+		protected override Task OnInit()
+		{
+			svg = new SKSvg();
+			using (var stream = SampleMedia.Images.LogosSvg)
+				svg.Load(stream);
+
+			return base.OnInit();
+		}
+
+		protected override void OnDrawSample(SKCanvas canvas, int width, int height)
+		{
+			canvas.Clear(SKColors.White);
+
+			float canvasMin = Math.Min(width, height);
+			float svgMax = Math.Max(svg.Picture.Bounds.Width, svg.Picture.Bounds.Height);
+			float scale = canvasMin / svgMax;
+			var matrix = SKMatrix.MakeScale(scale, scale);
+
+			canvas.DrawPicture(svg.Picture, ref matrix);
+		}
+	}
+}
