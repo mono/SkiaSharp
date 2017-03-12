54372ac153e054418998aae5290d532a547c9967
Matthew Leibowitz <mattleibow@live.com>
2017-03-12 10:11:24 +0200

    Mac bash is too old

--- bootstrapper.sh
+++ bootstrapper.sh
@@ -105,7 +105,8 @@ else
     mono $CAKE_EXE $SCRIPT -verbosity=$VERBOSITY -configuration=$CONFIGURATION -target=$TARGET $EXTRA_ARGS
 fi
 
-if [ "${TARGET,,}" == "clean" ] && [ $? -eq 0 ]; then
+TEMP_TARGET=`echo "${TARGET}" | tr '[A-Z]' '[a-z]'`
+if [ "$TEMP_TARGET" == "clean" ] && [ $? -eq 0 ]; then
     echo "Removing cake bits too..."
     rm -rf "$TOOLS_DIR/Addins/" "$TOOLS_DIR/Cake/"
     rm -rf "$TOOLS_DIR/Microsoft.DotNet.BuildTools.GenAPI/"
