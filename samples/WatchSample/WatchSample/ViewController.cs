bd6533b21c55ca322c53a7d2b2ad7827540db5ed
Matthew Leibowitz <mattleibow@live.com>
2017-08-31 15:41:24 +0200

    Added a watchOS sample

---
+++ samples/WatchSample/WatchSample/ViewController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UIKit;
+
+namespace WatchSample
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle)
+			: base(handle)
+		{
+		}
+	}
+}
