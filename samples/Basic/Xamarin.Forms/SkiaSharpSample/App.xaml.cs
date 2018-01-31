75a85e5c086f8974e0a9ff05c407bfcf7c62f0ef
Matthew Leibowitz <mattleibow@live.com>
2018-01-31 05:13:51 +0200

    Added basic Xamarin.Forms sample

---
+++ samples/Basic/Xamarin.Forms/SkiaSharpSample/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace SkiaSharpSample
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+	}
+}
