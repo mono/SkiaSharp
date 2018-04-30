aa1e4d56cd8f0dc2812168e29390b799eaeb91f0
Matthew Leibowitz <mattleibow@live.com>
2018-04-30 15:49:34 +0200

    Add a script to help install the Tizen SDK

---
+++ install-tizen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir ~/tizen-temp
+cd ~/tizen-temp
+
+curl -L -o "tizen-install.bin" http://download.tizen.org/sdk/Installer/tizen-studio_2.3/web-cli_Tizen_Studio_2.3_macos-64.bin
+chmod +x tizen-install.bin
+./tizen-install.bin --accept-license ~/tizen-studio
+
+ls -l ~/tizen-studio
+
+cd ~/tizen-studio
+./package-manager/package-manager-cli.bin install --accept-license MOBILE-4.0
