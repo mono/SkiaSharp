e42b6763d5830ed48191d189b37e64bc616aea0b
Matthew Leibowitz <mattleibow@live.com>
2018-05-02 12:56:50 +0200

    Download and install java

--- install-tizen.sh
+++ install-tizen.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
+# download java
+mkdir ~/java-temp
+cd ~/java-temp
+curl -L -o "jdk.tar.gz" http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz
+
+# extract java
+tar xvf jdk.tar.gz -C ../
+
+# add java to the path
+PATH=$PATH:~/jdk-10/bin
+
+# download tizen
 mkdir ~/tizen-temp
 cd ~/tizen-temp
-
-# mac:   http://download.tizen.org/sdk/Installer/tizen-studio_2.3/web-cli_Tizen_Studio_2.3_macos-64.bin
-# linux: http://download.tizen.org/sdk/Installer/tizen-studio_2.3/web-cli_Tizen_Studio_2.3_ubuntu-64.bin
-
 curl -L -o "tizen-install.bin" http://download.tizen.org/sdk/Installer/tizen-studio_2.3/web-cli_Tizen_Studio_2.3_ubuntu-64.bin
+
+# install tizen
 chmod +x tizen-install.bin
 ./tizen-install.bin --accept-license ~/tizen-studio
 
-ls -l ~/tizen-studio
-
+# install packages
 cd ~/tizen-studio
 ./package-manager/package-manager-cli.bin install --accept-license MOBILE-4.0
