d570cbe0d722e8cd90e5e46672d107eeccd659da
Matthew Leibowitz <mattleibow@live.com>
2018-05-09 01:18:28 +0200

    Trying to fix the test that always fails...

--- tests/Tests/SKCodecTest.cs
+++ tests/Tests/SKCodecTest.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SkiaSharp.Tests
@@ -224,12 +225,11 @@ namespace SkiaSharp.Tests
 		}
 	
 		[SkippableFact]
-		public void DownloadedStream ()
+		public async Task DownloadedStream ()
 		{
 			var httpClient = new HttpClient ();
-			using (var stream = httpClient.GetStreamAsync (new Uri ("http://www.gstatic.com/webp/gallery/2.webp")).Result)
-			using (var nonSeekable = new NonSeekableReadOnlyStream (stream))
-			using (var bitmap = SKBitmap.Decode (nonSeekable))
+			using (var stream = await httpClient.GetStreamAsync (new Uri ("http://www.gstatic.com/webp/gallery/2.webp")))
+			using (var bitmap = SKBitmap.Decode (stream))
 				Assert.NotNull (bitmap);
 		}
 	
