3353a9eeb337aed241f7e734c24d169d6bd0d5a1
Matthew Leibowitz <mattleibow@live.com>
2018-07-20 00:00:59 +0200

    Added a script to build the gm sample app for testing

---
+++ scripts/build-gm.ps1
@@ -0,0 +1,18 @@
+$errorActionPreference = 'Stop'
+
+try {
+    cd externals/skia
+
+    # git-sync-deps
+    python tools/git-sync-deps
+
+    # gn
+    ./bin/gn gen "out/Debug"
+
+    # ninja
+    ../../externals/depot_tools/ninja "gm" -C "out/Debug"
+} finally {
+    cd ../../
+}
+
+exit $LASTEXITCODE
---
+++ scripts/build-gm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# git-sync-deps
+(cd externals/skia && python tools/git-sync-deps)
+
+# gn
+(cd externals/skia && ./bin/gn gen "out/Debug")
+
+# ninja
+./externals/depot_tools/ninja 'gm' -C "externals/skia/out/Debug"
