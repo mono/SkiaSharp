d22e2b56b2b79fabb7d6c453ed95688f1d53136c
Matthew Leibowitz <mattleibow@live.com>
2018-08-11 16:22:17 +0200

    a

--- scripts/pipeline.groovy
+++ scripts/pipeline.groovy
@@ -102,8 +102,10 @@ properties([
     compressBuildLog()
 ])
 
-echo "env"
-echo "${env}"
+node {
+    echo(env.getEnvironment().collect({environmentVariable ->  "${environmentVariable.key} = ${environmentVariable.value}"}).join("\n"))
+    echo(System.getenv().collect({environmentVariable ->  "${environmentVariable.key} = ${environmentVariable.value}"}).join("\n"))
+}
 
 // // run all the native builds
 // parallel([
