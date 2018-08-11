b4018ac8469476b30b45baa607a49114a9517d84
Matthew Leibowitz <mattleibow@live.com>
2018-08-11 18:51:13 +0200

    wasd

--- scripts/pipeline.groovy
+++ scripts/pipeline.groovy
@@ -26,48 +26,53 @@ def cmdResult(script) {
     }
 }
 
+def getWSRoot() {
+    cleanBranch = env.BRANCH_NAME.replace("/", "_").replace("\\", "_")
+    wsRoot = "workspace"
+    if (!isUnix()) {
+        wsRoot = "C:/bld"
+    }
+    return "${wsRoot}/SkiaSharp/${cleanBranch}"
+}
+
 def createNativeBuilder(platform, host, label) {
+    githubContext = "Build Native - ${platform} on ${host}"
+
     return {
         node(label) {
             timestamps {
-                def githubContext = "Build Native - ${platform} on ${host}"
-                def cleanBranch = env.BRANCH_NAME.replace("/", "_").replace("\\", "_")
-                def cleanPlatform = platform.toLowerCase()
-
-                def wsRoot = "workspace"
-                if (!isUnix()) {
-                    wsRoot = "C:/bld"
-                }
-                ws("${wsRoot}/SkiaSharp/${cleanBranch}/${cleanPlatform}") {
-                    stage("Checkout Native") {
-                        // clone and checkout repository
-                        checkout scm
-
-                        // get current commit sha
-                        commitHash = cmdResult("git rev-parse HEAD").trim()
-
-                        // let GitHub know we are building
-                        reportGitHubStatus(commitHash, githubContext, env.BUILD_URL, "PENDING", "Building...")
-                    }
-
+                ws("${getWSRoot()}/Native-${platform.toLowerCase()}") {
                     try {
+                        stage("Setup Native") {
+                            reportGitHubStatus(commitHash, githubContext, env.BUILD_URL, "PENDING", "Building...")
+                        }
+
+                        stage("Checkout Native") {
+                            // checkout scm
+                            // cmd("git submodule update --init --recursive")
+                        }
+
                         stage("Build Native") {
-                            if (host.toLowerCase() == "linux") {
-                                install_tizen = ""
-                                if (platform.toLowerCase() == "tizen") {
-                                    install_tizen = "./scripts/install-tizen.sh && "
-                                }
-                                chroot(
-                                    chrootName: "${label}-stable",
-                                    command: "bash ${install_tizen} ./bootstrapper.sh -t externals-${platform.toLowerCase()} -v normal",
-                                    additionalPackages: "xvfb xauth libfontconfig1-dev libglu1-mesa-dev g++ mono-complete msbuild curl ca-certificates-mono unzip python git referenceassemblies-pcl dotnet-sdk-2.0.0 ttf-ancient-fonts openjdk-8-jdk zip gettext openvpn acl libxcb-render-util0 libv4l-0 libsdl1.2debian libxcb-image0 bridge-utils rpm2cpio libxcb-icccm4 libwebkitgtk-1.0-0 cpio")
-                            } else if (host.toLowerCase() == "macos") {
-                                sh("bash ./bootstrapper.sh -t externals-${platform.toLowerCase()} -v normal")
-                            } else if (host.toLowerCase() == "windows") {
-                                powershell(".\\bootstrapper.ps1 -t externals-${platform.toLowerCase()} -v normal")
-                            } else {
-                                throw new Exception("Unknown host platform: ${host}")
-                            }
+                            touch "output/native/${host}/${platform}/dummy.txt"
+                            // target = "externals-${platform.toLowerCase()}"
+                            // verbosity = "normal"
+
+                            // if (host.toLowerCase() == "linux") {
+                            //     install_tizen = ""
+                            //     if (platform.toLowerCase() == "tizen") {
+                            //         install_tizen = "./scripts/install-tizen.sh && "
+                            //     }
+                            //     chroot(
+                            //         chrootName: "${label}-stable",
+                            //         command: "bash ${install_tizen} ./bootstrapper.sh -t ${target} -v ${verbosity}",
+                            //         additionalPackages: "xvfb xauth libfontconfig1-dev libglu1-mesa-dev g++ mono-complete msbuild curl ca-certificates-mono unzip python git referenceassemblies-pcl dotnet-sdk-2.0.0 ttf-ancient-fonts openjdk-8-jdk zip gettext openvpn acl libxcb-render-util0 libv4l-0 libsdl1.2debian libxcb-image0 bridge-utils rpm2cpio libxcb-icccm4 libwebkitgtk-1.0-0 cpio")
+                            // } else if (host.toLowerCase() == "macos") {
+                            //     sh("bash ./bootstrapper.sh -t ${target} -v ${verbosity}")
+                            // } else if (host.toLowerCase() == "windows") {
+                            //     powershell(".\\bootstrapper.ps1 -t ${target} -v ${verbosity}")
+                            // } else {
+                            //     throw new Exception("Unknown host platform: ${host}")
+                            // }
                         }
 
                         stage("Upload Native") {
@@ -89,7 +94,96 @@ def createNativeBuilder(platform, host, label) {
                                 uploadZips: false,
                                 virtualPath: "ArtifactsFor-${env.BUILD_NUMBER}/${commitHash}/${platform.toLowerCase()}_${host.toLowerCase()}"
                             ])
+                        }
 
+                        stage("Teardown Native") {
+                            reportGitHubStatus(commitHash, githubContext, env.BUILD_URL, "SUCCESS", "Build complete.")
+                        }
+                    } catch (Exception e) {
+                        reportGitHubStatus(commitHash, githubContext, env.BUILD_URL, "FAILURE", "Build failed.")
+                        throw e
+                    }
+                }
+            }
+        }
+    }
+}
+
+def createManagedBuilder(host, label) {
+    githubContext = "Build Managed - ${host}"
+
+    return {
+        node(label) {
+            timestamps {
+                ws("${getWSRoot()}/Managed-${host.toLowerCase()}") {
+                    try {
+                        stage("Setup Managed") {
+                            reportGitHubStatus(commitHash, githubContext, env.BUILD_URL, "PENDING", "Building...")
+                        }
+
+                        stage("Checkout Managed") {
+                            // checkout scm
+                        }
+
+                        stage("Download Native") {
+                            step([
+                                $class: "AzureStorageBuilder",
+                                downloadType: [
+                                    value: "container",
+                                    containerName: "SkiaSharp-Public-Artifacts",
+                                ],
+                                includeFilesPattern: "ArtifactsFor-${env.BUILD_NUMBER}/${commitHash}/**/*",
+                                excludeFilesPattern: "",
+                                downloadDirLoc: "",
+                                flattenDirectories: false,
+                                includeArchiveZips: false,
+                                strAccName: "credential for xamjenkinsartifact",
+                                storageCredentialId: "fbd29020e8166fbede5518e038544343"
+
+                            ])
+                        }
+
+                        stage("Build Managed") {
+                            touch "output/managed/${host}/dummy.txt"
+                            // target = "Everything"
+                            // verbosity = "normal"
+
+                            // if (host.toLowerCase() == "linux") {
+                            //     chroot(
+                            //         chrootName: "${label}-stable",
+                            //         command: "bash ./bootstrapper.sh -t ${target} -v ${verbosity}",
+                            //         additionalPackages: "xvfb xauth libfontconfig1-dev libglu1-mesa-dev g++ mono-complete msbuild curl ca-certificates-mono unzip python git referenceassemblies-pcl dotnet-sdk-2.0.0 ttf-ancient-fonts openjdk-8-jdk zip gettext openvpn acl libxcb-render-util0 libv4l-0 libsdl1.2debian libxcb-image0 bridge-utils rpm2cpio libxcb-icccm4 libwebkitgtk-1.0-0 cpio")
+                            // } else if (host.toLowerCase() == "macos") {
+                            //     sh("bash ./bootstrapper.sh -t ${target} -v ${verbosity}")
+                            // } else if (host.toLowerCase() == "windows") {
+                            //     powershell(".\\bootstrapper.ps1 -t ${target} -v ${verbosity}")
+                            // } else {
+                            //     throw new Exception("Unknown host platform: ${host}")
+                            // }
+                        }
+
+                        stage("Upload Managed") {
+                            fingerprint("output/**/*")
+                            step([
+                                $class: "WAStoragePublisher",
+                                allowAnonymousAccess: true,
+                                cleanUpContainer: false,
+                                cntPubAccess: true,
+                                containerName: "SkiaSharp-Public-Artifacts",
+                                doNotFailIfArchivingReturnsNothing: false,
+                                doNotUploadIndividualFiles: false,
+                                doNotWaitForPreviousBuild: true,
+                                excludeFilesPath: "",
+                                filesPath: "output/**/*",
+                                storageAccName: "credential for xamjenkinsartifact",
+                                storageCredentialId: "fbd29020e8166fbede5518e038544343",
+                                uploadArtifactsOnlyIfSuccessful: false,
+                                uploadZips: false,
+                                virtualPath: "ArtifactsFor-${env.BUILD_NUMBER}/${commitHash}/${host.toLowerCase()}"
+                            ])
+                        }
+
+                        stage("Teardown Managed") {
                             reportGitHubStatus(commitHash, githubContext, env.BUILD_URL, "SUCCESS", "Build complete.")
                         }
                     } catch (Exception e) {
@@ -105,6 +199,16 @@ def createNativeBuilder(platform, host, label) {
 properties([
     compressBuildLog()
 ])
+
+// prepare the process
+node("ubuntu-1604-amd64") {
+    timestamps {
+        stage("Setup") {
+            checkout scm
+            commitHash = cmdResult("git rev-parse HEAD").trim()
+        }
+    }
+}
 
 // run all the native builds
 parallel([
@@ -128,8 +232,24 @@ parallel([
 ])
 
 // run all the managed builds
-// def managedBuilders = [:]
-// nativeBuilders["macos"]             = createNativeBuilder("macOS",      "macOS",    "components")
-// parallel managedBuilders
+parallel ([
+    linux: createManagedBuilder("Linux",    "ubuntu-1604-amd64")
+])
 
 // run the packaging
+node("ubuntu-1604-amd64") {
+    timestamps {
+        stage("Packing") {
+            // checkout scm
+        }
+    }
+}
+
+// clean up
+node("ubuntu-1604-amd64") {
+    timestamps {
+        stage("Teardown") {
+            // checkout scm
+        }
+    }
+}
