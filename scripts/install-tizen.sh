a747dabbcca9553083680c80acddceb5bbcd90fa
Matthew Leibowitz <mattleibow@live.com>
2019-02-02 05:49:28 +0200

    Updated the tizen install script to work on macos and linux

--- scripts/install-tizen.sh
+++ scripts/install-tizen.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# download tizen
 mkdir -p ~/tizen-temp
 cd ~/tizen-temp
-curl -L -o "tizen-install.bin" http://download.tizen.org/sdk/Installer/tizen-studio_2.4/web-cli_Tizen_Studio_2.4_ubuntu-64.bin
+
+# detect platform
+if [ "$(uname)" == "Darwin" ]; then
+    platform="macos-64"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    platform="ubuntu-64"
+else
+    exit 1
+fi
+
+# download tizen
+curl -L -o "tizen-install.bin" "http://download.tizen.org/sdk/Installer/tizen-studio_2.4/web-cli_Tizen_Studio_2.4_$platform.bin"
 
 # install tizen
 chmod +x tizen-install.bin
