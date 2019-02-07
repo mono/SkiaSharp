9601e520a4c43679a47425a98469026490f6a0c2
Matthew Leibowitz <mattleibow@live.com>
2019-02-07 17:42:02 +0200

    Create provision.ps1

---
+++ scripts/provision.ps1
@@ -0,0 +1,13 @@
+$errorActionPreference = 'Stop'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$uri = "https://api.github.com/repos/xamarin/components/contents/provisionator/bootstrapinator.ps1"
+$headers = @{
+  "Authorization"="token $env:GITHUB_TOKEN";
+  "Accept"="application/vnd.github.v3.raw"
+}
+
+Invoke-WebRequest -Uri $uri -Headers $headers -OutFile "bootstrapinator.ps1"
+
+& .\bootstrapinator.ps1 -executeScript "provision-skiasharp.csx"
+& .\provisionator.ps1 "provision-skiasharp.csx"
