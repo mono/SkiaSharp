619f0abbe7ec98895fba0de1de617f988ad991c5
Matthew Leibowitz <mattleibow@live.com>
2019-02-08 08:08:38 +0200

    Update provision.ps1

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -1,17 +1,22 @@
 $errorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+Write-Host "Downloading..."
 $headers = @{
   "Authorization"="token $env:GITHUB_TOKEN";
   "Accept"="application/vnd.github.v3.raw"
 }
-
 Invoke-WebRequest -Uri "$env:PROVISIONATOR_URL" -Headers $headers -OutFile "bootstrapinator.ps1"
 
+Write-Host "Preparing..."
 & .\bootstrapinator.ps1 -executeScript "provision-skiasharp.csx" "$env:GITHUB_TOKEN"
 
+Write-Host "Authorizing..."
 if ($IsMacOS) {
   & .\provisionator.ps1 keychain set github.com "$env:GITHUB_TOKEN"
 }
 
+Write-Host "Executing..."
 & .\provisionator.ps1 "provision-skiasharp.csx"
+
+Write-Host "Complete."
