5377b4bf36e362abc918a04e962d90e2890178b7
Benedikt Schroeder <Gillibald@users.noreply.github.com>
2019-06-22 21:05:28 +0200

    Update HarfBuzz to v2.5.2 (#872)
    Switched to clang for Android and Linux

--- native-builds/libHarfBuzzSharp_osx/config.h
+++ native-builds/libHarfBuzzSharp_osx/config.h
@@ -24,13 +24,13 @@
 /* #undef HAVE_DIRECTWRITE */
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
+/* #undef HAVE_DLFCN_H */
 
 /* Define to 1 if you have the <dwrite.h> header file. */
 /* #undef HAVE_DWRITE_H */
 
 /* Have simple TrueType Layout backend */
-#define HAVE_FALLBACK 1
+/* #undef HAVE_FALLBACK */
 
 /* Have fontconfig library */
 /* #undef HAVE_FONTCONFIG */
@@ -137,9 +137,6 @@
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
-/* Have UCDN Unicode functions */
-#define HAVE_UCDN 1
-
 /* Have Uniscribe library */
 /* #undef HAVE_UNISCRIBE */
 
@@ -165,7 +162,7 @@
 #define PACKAGE_NAME "HarfBuzz"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "HarfBuzz 2.3.1"
+#define PACKAGE_STRING "HarfBuzz 2.5.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "harfbuzz"
@@ -174,7 +171,7 @@
 #define PACKAGE_URL "http://harfbuzz.org/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.3.1"
+#define PACKAGE_VERSION "2.5.2"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
