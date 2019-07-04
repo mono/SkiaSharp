ec786d56098c7dcd9665fd48f07637886f437c4c
Davide Tuccilli <davide@gritworld.com>
2019-07-04 12:00:25 +0200

    Encapsulated fields of marshaled struct into properties

--- binding/Binding/GRVkAlloc.cs
+++ binding/Binding/GRVkAlloc.cs
@@ -1,16 +1,35 @@
 ﻿using System.Runtime.InteropServices;
+using VkDeviceMemory = SkiaSharp.VkNonDispatchableHandle;
+using VkDeviceSize = System.UInt64;
 
 namespace SkiaSharp
 {
-	using VkDeviceMemory = VkNonDispatchableHandle;
-	using VkDeviceSize = System.UInt64;
-
 	[StructLayout (LayoutKind.Sequential)]
 	public struct GRVkAlloc
 	{
-		public VkDeviceMemory Memory;
-		public VkDeviceSize Offset;
-		public VkDeviceSize Size;
-		public uint Flags;
+		private VkDeviceMemory memory;
+		private VkDeviceSize offset;
+		private VkDeviceSize size;
+		private uint flags;
+
+		public VkNonDispatchableHandle Memory {
+			get => memory;
+			set => memory = value;
+		}
+
+		public ulong Offset {
+			get => offset;
+			set => offset = value;
+		}
+
+		public ulong Size {
+			get => size;
+			set => size = value;
+		}
+
+		public uint Flags {
+			get => flags;
+			set => flags = value;
+		}
 	}
 }
--- binding/Binding/GRVkImageInfo.cs
+++ binding/Binding/GRVkImageInfo.cs
@@ -1,20 +1,49 @@
 ﻿using System.Runtime.InteropServices;
+using VkImage = SkiaSharp.VkNonDispatchableHandle;
+using VkImageTiling = System.Int32;
+using VkImageLayout = System.Int32;
+using VkFormat = System.Int32;
 
 namespace SkiaSharp
 {
-	using VkImage = VkNonDispatchableHandle;
-	using VkImageTiling = System.Int32;
-	using VkImageLayout = System.Int32;
-	using VkFormat = System.Int32;
-
 	[StructLayout (LayoutKind.Sequential)]
 	public struct GRVkImageInfo
 	{
-		public VkImage Image;
-		public GRVkAlloc Alloc;
-		public VkImageTiling ImageTiling;
-		public VkImageLayout ImageLayout;
-		public VkFormat Format;
-		public uint LevelCount;
+		private VkImage image;
+		private GRVkAlloc alloc;
+		private VkImageTiling imageTiling;
+		private VkImageLayout imageLayout;
+		private VkFormat format;
+		private uint levelCount;
+
+		public VkNonDispatchableHandle Image {
+			get => image;
+			set => image = value;
+		}
+
+		public GRVkAlloc Alloc {
+			get => alloc;
+			set => alloc = value;
+		}
+
+		public int ImageTiling {
+			get => imageTiling;
+			set => imageTiling = value;
+		}
+
+		public int ImageLayout {
+			get => imageLayout;
+			set => imageLayout = value;
+		}
+
+		public int Format {
+			get => format;
+			set => format = value;
+		}
+
+		public uint LevelCount {
+			get => levelCount;
+			set => levelCount = value;
+		}
 	}
 }
