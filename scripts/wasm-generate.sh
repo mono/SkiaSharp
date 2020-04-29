2f1ace1ee6734cfd8190f0b0c3336bbefd316fa2
Jerome Laban <jerome.laban@nventive.com>
2020-04-29 16:41:16 -0400

    Update skia module to latest

--- scripts/wasm-generate.sh
+++ scripts/wasm-generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $BUILD_SOURCESDIRECTORY
+cd $BUILD_SOURCESDIRECTORY/externals/skia
 
 # Disable strict mode, as we don't use the output of CanvasKit
 sed -i 's/STRICT=1/STRICT=0/g' modules/canvaskit/compile.sh
@@ -9,7 +9,7 @@ modules/canvaskit/compile.sh
 
 export ADDITIONAL_OBJ=out/canvaskit_wasm/obj/libAdditional
 
-md $ADDITIONAL_OBJ
+mkdir -p $ADDITIONAL_OBJ
 emcc -std=c++17 -I. modules/canvaskit/fonts/NotoMono-Regular.ttf.cpp -r -o $ADDITIONAL_OBJ/NotoMonoRegularttf.o
 emcc -std=c++17 -I. src/xml/SkXMLWriter.cpp -r -o $ADDITIONAL_OBJ/SkXMLWriter.o
 emcc -std=c++17 -I. src/svg/SkSVGCanvas.cpp -r -o $ADDITIONAL_OBJ/SkSVGCanvas.o
