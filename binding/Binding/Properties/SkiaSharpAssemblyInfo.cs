f1c671ef74c880efd622112b427252abf1dafb53
Matthew Leibowitz <mattleibow@live.com>
2020-05-08 02:22:25 +0200

    Correct company name

--- binding/Binding/Properties/SkiaSharpAssemblyInfo.cs
+++ binding/Binding/Properties/SkiaSharpAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("SkiaSharp")]
 [assembly: AssemblyDescription("SkiaSharp is a cross-platform 2D graphics API for .NET platforms that can be used across mobile, server and desktop models to render images.")]
-[assembly: AssemblyCompany("Xamarin Inc.")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("SkiaSharp")]
 [assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
 [assembly: NeutralResourcesLanguage("en")]
