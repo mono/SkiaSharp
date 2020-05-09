877e1805079ebcf07acd40028f2f5ead3665a370
Matthew Leibowitz <mattleibow@live.com>
2020-05-09 14:05:37 +0200

    repeat on mac

--- scripts/install-tizen.ps1
+++ scripts/install-tizen.ps1
@@ -12,12 +12,15 @@ $ErrorActionPreference = 'Stop'
 if ($IsMacOS) {
     $platform = "macos-64"
     $ext = "bin"
+    $repeat = 2
 } elseif ($IsLinux) {
     $platform = "ubuntu-64"
     $ext = "bin"
+    $repeat = 1
 } else {
     $platform = "windows-64"
     $ext = "exe"
+    $repeat = 1
 }
 
 $url = "http://download.tizen.org/sdk/Installer/tizen-studio_${Version}/web-cli_Tizen_Studio_${Version}_${platform}.${ext}"
@@ -54,14 +57,7 @@ if ($IsMacOS -or $IsLinux) {
 # install packages
 Write-Host "Installing Additional Packages: '$packages'..."
 $packMan = Join-Path (Join-Path "$ts" "package-manager") "package-manager-cli.${ext}"
-try {
-    if ($IsMacOS -or $IsLinux) {
-        & "bash" "$packMan" install --no-java-check --accept-license "$packages"
-    } else {
-        & "$packMan" install --no-java-check --accept-license "$packages"
-    }
-} catch {
-    # just retry and see if that works
+for($i = 0; $i -lt $repeat; $i++) {
     if ($IsMacOS -or $IsLinux) {
         & "bash" "$packMan" install --no-java-check --accept-license "$packages"
     } else {
