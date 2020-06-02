72ddbef93c75591fb45d7b24f2876891ab3319e3
Matthew Leibowitz <mattleibow@live.com>
2020-06-02 03:25:02 +0200

    Updated more samples

--- samples/Basic/Desktop/SkiaSharpSample/Form1.Designer.cs
+++ samples/Basic/Desktop/SkiaSharpSample/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.skiaView = new SkiaSharp.Views.Desktop.SKControl();
 			this.SuspendLayout();
 			// 
@@ -47,8 +48,9 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
 			this.ClientSize = new System.Drawing.Size(774, 529);
 			this.Controls.Add(this.skiaView);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "SkiaSharp";
 			this.ResumeLayout(false);
 
 		}
