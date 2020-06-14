dd620b2aa94041a2b3843ea8cc5d94b15034aabe
Matthew Leibowitz <mattleibow@live.com>
2020-06-14 20:24:59 +0200

    Remove unused things

--- samples/Basic/Wasm/SkiaSharpSample/Program.cs
+++ samples/Basic/Wasm/SkiaSharpSample/Program.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.IO;
-using Windows.ApplicationModel.Activation;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Shapes;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Uno.Extensions;
-using Uno.Foundation;
-using Uno.Logging;
-using Windows.UI.Popups;
-
+using Uno.UI;
+using Windows.UI.Xaml;
 
 namespace SkiaSharpSample
 {
@@ -19,72 +11,68 @@ namespace SkiaSharpSample
 
 		static int Main(string[] args)
 		{
-			Uno.UI.FeatureConfiguration.UIElement.AssignDOMXamlName = true;
-
-			WebAssemblyRuntime.InvokeJS("Uno.UI.Demo.Analytics.reportPageView('main');");
+			FeatureConfiguration.UIElement.AssignDOMXamlName = true;
 
 			ConfigureFilters(LogExtensionPoint.AmbientLoggerFactory);
-			  
-			Windows.UI.Xaml.Application.Start(_ => _app = new App());
+
+			Application.Start(_ => _app = new App());
 
 			return 0;
 		}
+
 		static void ConfigureFilters(ILoggerFactory factory)
 		{
 			factory
 				.WithFilter(new FilterLoggerSettings
-					{
-						{ "Uno", LogLevel.Warning },
-						{ "Windows", LogLevel.Warning },
-						{ "nVentive.Umbrella", LogLevel.Warning },
-						{ "SkiaSharp", LogLevel.Warning },
-						// { "ShiaTSInteropMarshaller", LogLevel.Warning },
+				{
+					{ "Uno", LogLevel.Warning },
+					{ "Windows", LogLevel.Warning },
+					{ "nVentive.Umbrella", LogLevel.Warning },
+					{ "SkiaSharp", LogLevel.Warning },
 
-						// View Navigation related messages
-						// { "nVentive.Umbrella.Views.Navigation", LogLevel.Debug },
-						// { "nVentive.Umbrella.Presentation.Light.ViewModelRegistry", LogLevel.Debug },
-						   
-						// All presentation related messages
-						// { "nVentive.Umbrella.Presentation.Light", LogLevel.Debug },
-						   
-						// ViewModel related messages
-						// { "nVentive.Umbrella.Presentation.Light.ViewModelBase", LogLevel.Debug },
-						   
-						// Dynamic properties related messages
-						// { "nVentive.Umbrella.Presentation.Light.DynamicProperties", LogLevel.Debug },
-						   
-						// AVVM/AVP Related messages
-						// { "nVentive.Umbrella.Presentation.Light.Extensions.AsyncValueViewModel", LogLevel.Debug },
-						// { "nVentive.Umbrella.Views.Controls.AsyncValuePresenter", LogLevel.Debug },
-						// { "nVentive.Umbrella.Views.Controls.IfDataContext", LogLevel.Debug },
-						   
-						// Layouter specific messages
-						//{ "Windows.UI.Xaml.Controls", LogLevel.Debug },
-						//{ "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
-						//{ "Windows.UI.Xaml.Controls.Panel", LogLevel.Debug },
-						   
-						// Binding related messages
-						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
-						   
-						//  Binder memory references tracking
-						// { "ReferenceHolder", LogLevel.Debug },
-						   
-						// Web Queries related messages
-						// gManager.SetLoggerLevel("nVentive.Umbrella.Web", LogLevel.Debug },
-						   
-						// Location service messages
-						// { "nVentive.Umbrella.Services.Location", LogLevel.Debug },
-						   
-						// Map Component messages
-						// { "nVentive.Umbrella.Components.Maps", LogLevel.Debug },
-						   
-						//  ApplicationViewState
-						//  { "nVentive.Umbrella.Views.Services.ApplicationViewState.ApplicationViewStateService", LogLevel.Debug },
-					}
-				)
+					// View Navigation related messages
+					// { "nVentive.Umbrella.Views.Navigation", LogLevel.Debug },
+					// { "nVentive.Umbrella.Presentation.Light.ViewModelRegistry", LogLevel.Debug },
+
+					// All presentation related messages
+					// { "nVentive.Umbrella.Presentation.Light", LogLevel.Debug },
+
+					// ViewModel related messages
+					// { "nVentive.Umbrella.Presentation.Light.ViewModelBase", LogLevel.Debug },
+
+					// Dynamic properties related messages
+					// { "nVentive.Umbrella.Presentation.Light.DynamicProperties", LogLevel.Debug },
+
+					// AVVM/AVP Related messages
+					// { "nVentive.Umbrella.Presentation.Light.Extensions.AsyncValueViewModel", LogLevel.Debug },
+					// { "nVentive.Umbrella.Views.Controls.AsyncValuePresenter", LogLevel.Debug },
+					// { "nVentive.Umbrella.Views.Controls.IfDataContext", LogLevel.Debug },
+
+					// Layouter specific messages
+					// { "Windows.UI.Xaml.Controls", LogLevel.Debug },
+					// { "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
+					// { "Windows.UI.Xaml.Controls.Panel", LogLevel.Debug },
+
+					// Binding related messages
+					// { "Windows.UI.Xaml.Data", LogLevel.Debug },
+					// { "Windows.UI.Xaml.Data", LogLevel.Debug },
+
+					//  Binder memory references tracking
+					// { "ReferenceHolder", LogLevel.Debug },
+
+					// Web Queries related messages
+					// gManager.SetLoggerLevel("nVentive.Umbrella.Web", LogLevel.Debug },
+
+					// Location service messages
+					// { "nVentive.Umbrella.Services.Location", LogLevel.Debug },
+
+					// Map Component messages
+					// { "nVentive.Umbrella.Components.Maps", LogLevel.Debug },
+
+					// ApplicationViewState
+					// { "nVentive.Umbrella.Views.Services.ApplicationViewState.ApplicationViewStateService", LogLevel.Debug },
+				})
 				.AddConsole(LogLevel.Debug);
 		}
-
 	}
 }
