c9adf61d03b7d84b5ca73cca266d5837c2a6ffa1
Matthew Leibowitz <mattleibow@live.com>
2020-06-29 01:26:52 +0200

    fixes and things

--- tests/SkiaSharp.Wasm.Tests/Tests.cs
+++ tests/SkiaSharp.Wasm.Tests/Tests.cs
@@ -1,4 +1,6 @@
 ﻿using Xunit;
+using System;
+using System.Runtime.InteropServices;
 
 namespace SkiaSharp.Tests
 {
@@ -14,5 +16,18 @@ namespace SkiaSharp.Tests
 		{
 			Assert.True(false);
 		}
+
+		[Fact]
+		public void Interop()
+		{
+			var str = Marshal.PtrToStringAnsi(sk_version_get_string());
+			Assert.Equal("80.0", str);
+		}
+
+		[DllImport("libSkiaSharp", CallingConvention = CallingConvention.Cdecl)]
+		static extern IntPtr sk_version_get_string();
+
+		[DllImport("libSkiaSharp", CallingConvention = CallingConvention.Cdecl)]
+		static extern int sk_colortype_get_default_8888();
 	}
 }
