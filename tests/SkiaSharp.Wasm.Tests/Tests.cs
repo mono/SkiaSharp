3faa9344f97c5244c1c3c01138043087d4770b54
Matthew Leibowitz <mattleibow@live.com>
2020-06-29 02:27:45 +0200

    better tests

--- tests/SkiaSharp.Wasm.Tests/Tests.cs
+++ tests/SkiaSharp.Wasm.Tests/Tests.cs
@@ -4,30 +4,29 @@ using System.Runtime.InteropServices;
 
 namespace SkiaSharp.Tests
 {
-	public class Tests
+	public class PlaceholderTest
 	{
-		[Fact]
-		public void Passing()
-		{
-		}
+		private const string SKIA = "libSkiaSharp";
 
 		[Fact]
-		public void Failing()
-		{
-			Assert.True(false);
-		}
-
-		[Fact]
-		public void Interop()
+		public void CheckVersion()
 		{
 			var str = Marshal.PtrToStringAnsi(sk_version_get_string());
-			Assert.Equal("80.0", str);
+			var milestone = sk_version_get_milestone();
+			var increment = sk_version_get_increment();
+
+			Assert.True(milestone > 0);
+			Assert.True(increment >= 0);
+			Assert.Equal($"{milestone}.{increment}", str);
 		}
 
-		[DllImport("libSkiaSharp", CallingConvention = CallingConvention.Cdecl)]
+		[DllImport(SKIA, CallingConvention = CallingConvention.Cdecl)]
 		static extern IntPtr sk_version_get_string();
 
-		[DllImport("libSkiaSharp", CallingConvention = CallingConvention.Cdecl)]
-		static extern int sk_colortype_get_default_8888();
+		[DllImport(SKIA, CallingConvention = CallingConvention.Cdecl)]
+		static extern int sk_version_get_milestone();
+
+		[DllImport(SKIA, CallingConvention = CallingConvention.Cdecl)]
+		static extern int sk_version_get_increment();
 	}
 }
