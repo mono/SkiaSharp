0934adcfd9fac00cada6d7cd3f94db8f480eacf0
Matthew Leibowitz <mattleibow@live.com>
2020-07-18 19:28:14 +0200

    Add GPU views just in case
    These are not complete or working properly yet. We might not be able to merge with this. But, to make things easier for now, just commit

---
+++ source/SkiaSharp.Views.Uno/SkiaSharp.Views.Uno/SKSwapChainPanel.cs
@@ -0,0 +1,116 @@
+﻿#if INCLUDE_GPU_VIEWS
+using System;
+using Windows.ApplicationModel;
+using Windows.Graphics.Display;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace SkiaSharp.Views.UWP
+{
+	public partial class SKSwapChainPanel : FrameworkElement
+	{
+		private static readonly DependencyProperty ProxyVisibilityProperty =
+			DependencyProperty.Register(
+				"ProxyVisibility",
+				typeof(Visibility),
+				typeof(SKSwapChainPanel),
+				new PropertyMetadata(Visibility.Visible, OnVisibilityChanged));
+
+		private static bool designMode = DesignMode.DesignModeEnabled;
+
+		private bool isVisible = true;
+
+		// workaround for https://github.com/mono/SkiaSharp/issues/1118
+		private int loadUnloadCounter = 0;
+
+		private void Initialize()
+		{
+			if (designMode)
+				return;
+
+			var display = DisplayInformation.GetForCurrentView();
+			OnDpiChanged(display);
+
+			Loaded += OnLoaded;
+			Unloaded += OnUnloaded;
+			SizeChanged += OnSizeChanged;
+
+			var binding = new Binding
+			{
+				Path = new PropertyPath(nameof(Visibility)),
+				Source = this
+			};
+			SetBinding(ProxyVisibilityProperty, binding);
+		}
+
+		public SKSize CanvasSize => lastSize;
+
+		public GRContext GRContext => context;
+
+		public event EventHandler<SKPaintGLSurfaceEventArgs> PaintSurface;
+
+		protected virtual void OnPaintSurface(SKPaintGLSurfaceEventArgs e)
+		{
+			PaintSurface?.Invoke(this, e);
+		}
+
+		private static void OnVisibilityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if (d is SKSwapChainPanel canvas && e.NewValue is Visibility visibility)
+			{
+				canvas.isVisible = visibility == Visibility.Visible;
+				canvas.Invalidate();
+			}
+		}
+
+		private void OnDpiChanged(DisplayInformation sender, object args = null)
+		{
+			Invalidate();
+		}
+
+		private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			Invalidate();
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			loadUnloadCounter++;
+			if (loadUnloadCounter != 1)
+				return;
+
+			DoLoaded();
+
+			var display = DisplayInformation.GetForCurrentView();
+			display.DpiChanged += OnDpiChanged;
+
+			OnDpiChanged(display);
+		}
+
+		private void OnUnloaded(object sender, RoutedEventArgs e)
+		{
+			loadUnloadCounter--;
+			if (loadUnloadCounter != 0)
+				return;
+
+			DoUnloaded();
+
+			var display = DisplayInformation.GetForCurrentView();
+			display.DpiChanged -= OnDpiChanged;
+		}
+
+		public void Invalidate()
+		{
+			if (Dispatcher.HasThreadAccess)
+				DoInvalidate();
+			else
+				Dispatcher.RunAsync(CoreDispatcherPriority.Normal, DoInvalidate);
+		}
+
+		partial void DoLoaded();
+
+		partial void DoUnloaded();
+	}
+}
+#edif
