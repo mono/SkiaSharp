2d0ac9aac3edd825a23d3299f4ac0fd33541c3b5
Matthew Leibowitz <mattleibow@live.com>
2020-07-24 23:12:46 +0200

    Add the iOS and macOS Views

---
+++ source/SkiaSharp.Views.Uno/SkiaSharp.Views.Uno.Mac/SKSwapChainPanel.macOS.cs
@@ -0,0 +1,45 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SkiaSharp.Views.UWP
+{
+	public partial class SKSwapChainPanel : FrameworkElement
+	{
+		private SKGLView glView;
+
+		public SKSwapChainPanel()
+		{
+			Initialize();
+		}
+
+		private SKSize GetCanvasSize() =>
+			glView?.CanvasSize ?? SKSize.Empty;
+
+		private GRContext GetGRContext() =>
+			glView?.GRContext;
+
+		partial void DoLoaded()
+		{
+			glView = new SKGLView(Bounds);
+			glView.PaintSurface += OnPaintSurface;
+			AddSubview(glView);
+		}
+
+		partial void DoUnloaded()
+		{
+			if (glView != null)
+			{
+				glView.RemoveFromSuperview();
+				glView.PaintSurface -= OnPaintSurface;
+				glView.Dispose();
+				glView = null;
+			}
+		}
+
+		private void DoInvalidate() =>
+			glView?.Display();
+
+		private void OnPaintSurface(object sender, SKPaintGLSurfaceEventArgs e) =>
+			OnPaintSurface(e);
+	}
+}
