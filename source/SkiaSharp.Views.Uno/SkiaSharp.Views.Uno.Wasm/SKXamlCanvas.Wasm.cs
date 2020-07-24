1fa3daddcc45873d6cd84c54392c6be64539b87d
Matthew Leibowitz <mattleibow@live.com>
2020-07-24 03:57:30 +0200

    Update the Uno package versions (#1426)

--- source/SkiaSharp.Views.Uno/SkiaSharp.Views.Uno.Wasm/SKXamlCanvas.Wasm.cs
+++ source/SkiaSharp.Views.Uno/SkiaSharp.Views.Uno.Wasm/SKXamlCanvas.Wasm.cs
@@ -11,29 +11,6 @@ namespace SkiaSharp.Views.UWP
 		private int pixelWidth;
 		private int pixelHeight;
 
-		static SKXamlCanvas()
-		{
-			const string js = @"
-(window || global).SkiaSharp_Views_UWP_SKXamlCanvas = class SKXamlCanvas {
-  static invalidateCanvas(pData, canvasId, width, height) {
-    var htmlCanvas = document.getElementById(canvasId);
-    htmlCanvas.width = width;
-    htmlCanvas.height = height;
-
-    var ctx = htmlCanvas.getContext('2d');
-    if (!ctx)
-      return false;
-
-    var buffer = new Uint8ClampedArray(Module.HEAPU8.buffer, pData, width * height * 4);
-    var imageData = new ImageData(buffer, width, height);
-    ctx.putImageData(imageData, 0, 0);
-
-    return true;
-  }
-};";
-			WebAssemblyRuntime.InvokeJS(js);
-		}
-
 		public SKXamlCanvas()
 			: base("canvas")
 		{
@@ -77,7 +54,7 @@ namespace SkiaSharp.Views.UWP
 				OnPaintSurface(new SKPaintSurfaceEventArgs(surface, info));
 			}
 
-			WebAssemblyRuntime.InvokeJS($"SkiaSharp_Views_UWP_SKXamlCanvas.invalidateCanvas({pixels}, \"{HtmlId}\", {info.Width}, {pixelHeight});");
+			WebAssemblyRuntime.InvokeJS($"SkiaSharp.Views.UWP.SKXamlCanvas.invalidateCanvas({pixels}, \"{HtmlId}\", {info.Width}, {pixelHeight});");
 		}
 
 		private unsafe void CreateBitmap(SKImageInfo info)
