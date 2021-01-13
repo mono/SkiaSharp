f1965d5c69f40a6288c48091b9933ff03c402d5d
Jerome Laban <jerome.laban@nventive.com>
2021-01-13 16:20:50 -0500

    feat: Add .NET 5 support

--- binding/SkiaSharp.Wasm/JSInterop.cs
+++ binding/SkiaSharp.Wasm/JSInterop.cs
@@ -9,34 +9,57 @@ using SkiaSharp;
 
 namespace WebAssembly
 {
-	[Obfuscation (Feature = "renaming", Exclude = true)]
 	internal sealed class Runtime
 	{
+		/// <summary>
+		/// Mono specific internal call.
+		/// </summary>
 		[MethodImpl (MethodImplOptions.InternalCall)]
 		private static extern string InvokeJS (string str, out int exceptional_result);
 
+		// Disable inlining to avoid the interpreter to evaluate an internal call that may not be available
+		[MethodImpl (MethodImplOptions.NoInlining)]
+		private static string MonoInvokeJS (string str, out int exceptionResult) => InvokeJS (str, out exceptionResult);
+
+		// Disable inlining to avoid the interpreter to evaluate an internal call that may not be available
+		[MethodImpl (MethodImplOptions.NoInlining)]
+		private static string NetCoreInvokeJS (string str, out int exceptionResult)
+			=> Interop.Runtime.InvokeJS (str, out exceptionResult);
+
+		/// <summary>
+		/// Invokes Javascript code in the hosting environment
+		/// </summary>
 		internal static string InvokeJS (string str)
 		{
-			var r = InvokeJS (str, out var exceptionResult);
+			var r = IsNetCore
+			? NetCoreInvokeJS (str, out var exceptionResult)
+			: MonoInvokeJS (str, out exceptionResult);
+
 			if (exceptionResult != 0) {
 				Console.Error.WriteLine ($"Error #{exceptionResult} \"{r}\" executing javascript: \"{str}\"");
 			}
 			return r;
 		}
-	}
 
-	namespace JSInterop
-	{ 
-		internal static class InternalCalls  
-		{ 
-			// Matches this signature:
-			// https://github.com/mono/mono/blob/f24d652d567c4611f9b4e3095be4e2a1a2ab23a4/sdks/wasm/driver.c#L21
-			[MethodImpl (MethodImplOptions.InternalCall)]
-			public static extern IntPtr InvokeJSUnmarshalled (out string exception, string functionIdentifier, IntPtr arg0, IntPtr arg1, IntPtr arg2);
-		}
+		internal static bool IsNetCore
+#if NET5_0
+			 => true;
+#else
+			 => Type.GetType ("System.Runtime.Loader.AssemblyLoadContext") != null;
+#endif
 	}
-} 
-   
+}
+
+internal sealed class Interop
+{
+	internal sealed class Runtime
+	{
+		[MethodImplAttribute (MethodImplOptions.InternalCall)]
+		public static extern string InvokeJS (string str, out int exceptional_result);
+	}
+}
+
+
 namespace SkiaSharp
 {
 	public static class WebAssemblyRuntime
