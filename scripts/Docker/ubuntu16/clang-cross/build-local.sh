b84c338da98b96e81d06e5c610b59eabc1d49752
Matthew Leibowitz <mattleibow@live.com>
2021-01-29 21:08:09 +0200

    fix issues

--- scripts/Docker/ubuntu16/clang-cross/build-local.sh
+++ scripts/Docker/ubuntu16/clang-cross/build-local.sh
@@ -3,10 +3,13 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ARCH="arm"
+ARCH=""
 BUILD_ARGS=""
-if [ "$1" ]; then
-    ARCH="$1"
+if [ "$1" == "arm" ]; then
+    ARCH="arm"
+    BUILD_ARGS="--build-arg TOOLCHAIN_ARCH=arm-linux-gnueabihf --build-arg TOOLCHAIN_ARCH_SHORT=arm"
+elif [ "$1" == "arm64" ]; then
+    ARCH="arm64"
     BUILD_ARGS="--build-arg TOOLCHAIN_ARCH=aarch64-linux-gnu --build-arg TOOLCHAIN_ARCH_SHORT=arm64"
 fi
 
