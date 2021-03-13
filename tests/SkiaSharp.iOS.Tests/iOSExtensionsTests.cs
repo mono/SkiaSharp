dfedaf14a5ab1eca6fa77981d62cae59d59af110
Matthew Leibowitz <mattleibow@live.com>
2021-03-13 18:30:49 +0200

    Add some iOS tests

---
+++ tests/SkiaSharp.iOS.Tests/iOSExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿//using Xunit;
+
+//namespace SkiaSharp.Views.iOS.Tests
+//{
+//	public class iOSExtensionsTests : iOSTests
+//	{
+//		[SkippableTheory]
+//		[InlineData(0)]
+//		[InlineData(10)]
+//		[InlineData(100)]
+//		[InlineData(255)]
+//		public void PixelBackedImageToBitmap(byte alpha)
+//		{
+//			using var bitmap = CreateTestBitmap(alpha);
+//			using var image = SKImage.FromBitmap(bitmap);
+
+//			using var iosBitmap = image.ToUIImage();
+
+//			ValidateTestBitmap(iosBitmap, alpha);
+
+//			androidBitmap.Recycle();
+//		}
+
+//		[SkippableTheory]
+//		[InlineData(0)]
+//		[InlineData(10)]
+//		[InlineData(100)]
+//		[InlineData(255)]
+//		public void BitmapToBitmap(byte alpha)
+//		{
+//			using var bitmap = CreateTestBitmap(alpha);
+
+//			using var androidBitmap = bitmap.ToBitmap();
+
+//			ValidateTestBitmap(androidBitmap, alpha);
+
+//			androidBitmap.Recycle();
+//		}
+
+//		[SkippableTheory]
+//		[InlineData(0)]
+//		[InlineData(10)]
+//		[InlineData(100)]
+//		[InlineData(255)]
+//		public void PixmapToBitmap(byte alpha)
+//		{
+//			using var bitmap = CreateTestBitmap(alpha);
+//			using var pixmap = bitmap.PeekPixels();
+
+//			using var androidBitmap = pixmap.ToBitmap();
+
+//			ValidateTestBitmap(androidBitmap, alpha);
+
+//			androidBitmap.Recycle();
+//		}
+
+//		[SkippableTheory]
+//		[InlineData(0)]
+//		[InlineData(10)]
+//		[InlineData(100)]
+//		[InlineData(255)]
+//		public void EncodedDataBackedImageToBitmap(byte alpha)
+//		{
+//			using var bitmap = CreateTestBitmap(alpha);
+//			using var data = bitmap.Encode(SKEncodedImageFormat.Png, 100);
+//			using var image = SKImage.FromEncodedData(data);
+
+//			using var androidBitmap = image.ToBitmap();
+
+//			ValidateTestBitmap(androidBitmap, alpha);
+
+//			androidBitmap.Recycle();
+//		}
+//	}
+//}
---
+++ tests/SkiaSharp.iOS.Tests/iOSTests.cs
@@ -0,0 +1,25 @@
+﻿//using SkiaSharp.Tests;
+//using Xunit;
+
+//namespace SkiaSharp.Views.iOS.Tests
+//{
+//	public abstract class iOSTests : SKTest
+//	{
+//		protected static void ValidateTestUIImage(UIImage uiImage, byte alpha = 255)
+//		{
+//			Assert.NotNull(bmp);
+//			Assert.Equal(40, bmp.Width);
+//			Assert.Equal(40, bmp.Height);
+
+//			Assert.Equal(Get(SKColors.Red), (uint)bmp.GetPixel(10, 10));
+//			Assert.Equal(Get(SKColors.Green), (uint)bmp.GetPixel(30, 10));
+//			Assert.Equal(Get(SKColors.Blue), (uint)bmp.GetPixel(10, 30));
+//			Assert.Equal(Get(SKColors.Yellow), (uint)bmp.GetPixel(30, 30));
+
+//			uint Get(SKColor color) =>
+//				alpha == 0
+//					? 0
+//					: (uint)color.WithAlpha(alpha);
+//		}
+//	}
+//}
