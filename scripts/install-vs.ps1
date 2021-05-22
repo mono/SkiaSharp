da49047c034c7e05f125c4ce2112584f9c6197c0
Matthew Leibowitz <mattleibow@live.com>
2021-05-22 07:09:05 +0200

    Onlu build net6 on windows
    mac/linux have an old msbuild

--- scripts/install-vs.ps1
+++ scripts/install-vs.ps1
@@ -19,12 +19,13 @@ Write-Host "Installing Visual Studio..."
 
 Remove-Item vs_community.exe
 
+$vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+
 Write-Host "Adding MSBuild to PATH..."
 $installationPath = & $vswhere -latest -prerelease -property installationPath
 Write-Host "##vso[task.prependpath]$installationPath\MSBuild\Current\Bin"
 
 Write-Host "Installed Visual Studio Versions:"
-$vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
 & $vswhere -all -prerelease -property installationPath
 
 Write-Host "Selected MSBuild Version:"
