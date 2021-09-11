8a594209b1d3e64e7262954da8fb35adf8a03344
Matthew Leibowitz <mattleibow@live.com>
2021-09-11 21:12:20 +0200

    new compiler, old gcc

--- scripts/install-tizen.ps1
+++ scripts/install-tizen.ps1
@@ -63,31 +63,31 @@ if ($IsMacOS -or $IsLinux) {
     & "$packMan" install --no-java-check --accept-license "$packages"
 }
 
-# function Swap-Tool {
-#     param ([string] $OldName, [string] $NewName)
-#
-#     $tsTools = Join-Path "$ts" "tools"
-#     $llvm40 = Join-Path $tsTools $OldName
-#     $llvm40OLD = Join-Path $tsTools "$OldName-OLD"
-#     $llvm10 = Join-Path $tsTools $NewName
-#     if (!(Test-Path $llvm40OLD)) {
-#         Copy-Item $llvm40 $llvm40OLD -Recurse -Force
-#     }
-#     if (Test-Path $llvm40) {
-#         Remove-Item $llvm40 -Recurse -Force
-#     }
-#     Copy-Item $llvm10 $llvm40 -Recurse -Force
-# }
-#
-# # Upgrade LLVM 4 to LLVM 10 by literally replacing the folder
-# if ($UpgradeLLVM) {
-#     Write-Host "Upgrading LLVM and GCC in place..."
-#     Swap-Tool "llvm-4.0.0" "llvm-10"
-#     Swap-Tool "aarch64-linux-gnu-gcc-6.2" "aarch64-linux-gnu-gcc-9.2"
-#     Swap-Tool "arm-linux-gnueabi-gcc-6.2" "arm-linux-gnueabi-gcc-9.2"
-#     Swap-Tool "i586-linux-gnueabi-gcc-6.2" "i586-linux-gnueabi-gcc-9.2"
-#     Swap-Tool "x86_64-linux-gnu-gcc-6.2" "x86_64-linux-gnu-gcc-9.2"
-# }
+function Swap-Tool {
+    param ([string] $OldName, [string] $NewName)
+
+    $tsTools = Join-Path "$ts" "tools"
+    $llvm40 = Join-Path $tsTools $OldName
+    $llvm40OLD = Join-Path $tsTools "$OldName-OLD"
+    $llvm10 = Join-Path $tsTools $NewName
+    if (!(Test-Path $llvm40OLD)) {
+        Copy-Item $llvm40 $llvm40OLD -Recurse -Force
+    }
+    if (Test-Path $llvm40) {
+        Remove-Item $llvm40 -Recurse -Force
+    }
+    Copy-Item $llvm10 $llvm40 -Recurse -Force
+}
+
+# Upgrade LLVM 4 to LLVM 10 by literally replacing the folder
+if ($UpgradeLLVM) {
+    Write-Host "Upgrading LLVM and GCC in place..."
+    Swap-Tool "llvm-4.0.0" "llvm-10"
+    # Swap-Tool "aarch64-linux-gnu-gcc-6.2" "aarch64-linux-gnu-gcc-9.2"
+    # Swap-Tool "arm-linux-gnueabi-gcc-6.2" "arm-linux-gnueabi-gcc-9.2"
+    # Swap-Tool "i586-linux-gnueabi-gcc-6.2" "i586-linux-gnueabi-gcc-9.2"
+    # Swap-Tool "x86_64-linux-gnu-gcc-6.2" "x86_64-linux-gnu-gcc-9.2"
+}
 
 # make sure that Tizen Studio is in TIZEN_STUDIO_HOME
 Write-Host "##vso[task.setvariable variable=TIZEN_STUDIO_HOME;]$ts";
