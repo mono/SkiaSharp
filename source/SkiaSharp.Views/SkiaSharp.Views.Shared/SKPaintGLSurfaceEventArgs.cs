78c43eb5f9f9d17dbb91737a15f04472f7703a16
Matthew Leibowitz <mattleibow@live.com>
2021-10-11 10:58:17 +0200

    Add versions

--- source/SkiaSharp.Views/SkiaSharp.Views.Shared/SKPaintGLSurfaceEventArgs.cs
+++ source/SkiaSharp.Views/SkiaSharp.Views.Shared/SKPaintGLSurfaceEventArgs.cs
@@ -75,7 +75,7 @@ namespace SkiaSharp.Views.Blazor
 
 #if !__BLAZOR__
 		[EditorBrowsable(EditorBrowsableState.Never)]
-		[Obsolete]
+		[Obsolete("Use SKPaintGLSurfaceEventArgs(SKSurface, GRBackendRenderTarget, GRSurfaceOrigin, SKColorType) instead.")]
 		public SKPaintGLSurfaceEventArgs(SKSurface surface, GRBackendRenderTarget renderTarget, GRSurfaceOrigin origin, SKColorType colorType, GRGlFramebufferInfo glInfo)
 		{
 			Surface = surface;
