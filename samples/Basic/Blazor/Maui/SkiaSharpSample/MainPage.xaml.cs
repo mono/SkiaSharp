a085032d2f6667c65656e2eb6552a6afccb8a5b0
Matthew Leibowitz <mattleibow@live.com>
2021-10-23 04:17:33 +0200

    Add support for Blazor desktop/mobile

---
+++ samples/Basic/Blazor/Maui/SkiaSharpSample/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace SkiaSharpSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ samples/Basic/Blazor/Maui/SkiaSharpSample/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace SkiaSharpSample
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+
+			builder
+				.RegisterBlazorMauiWebView()
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			builder.Services.AddBlazorWebView();
+
+			return builder.Build();
+		}
+	}
+}
