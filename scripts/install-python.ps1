ff3f01e610fb37c1f6b698366f287180deba9453
Matthew Leibowitz <mattleibow@live.com>
2022-02-20 23:37:50 +0800

    Download python

--- scripts/install-python.ps1
+++ scripts/install-python.ps1
@@ -5,6 +5,10 @@ Param(
 
 $ErrorActionPreference = 'Stop'
 
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$HOME_DIR = if ($env:HOME) { $env:HOME } else { $env:USERPROFILE }
+
 $destDir = Join-Path "$env:AGENT_TOOLSDIRECTORY" "Python" "$Version" "$Arch"
 $completeFile = "$destDir.complete"
 
@@ -15,15 +19,38 @@ if (Test-Path $completeFile) {
 
 Write-Host "Downloading manifest..."
 
-$uri = "https://raw.githubusercontent.com/actions/python-versions/main/versions-manifest.json"
+$pythonManifestUri = "https://raw.githubusercontent.com/actions/python-versions/main/versions-manifest.json"
+$pythonManifest = Invoke-WebRequest -Uri $pythonManifestUri | ConvertFrom-Json
 
-# Write-Host "Downloading Windows SDK Installer: $uri..."
-# .\scripts\download-file.ps1 -Uri $uri -OutFile sdksetup.exe
+if ($IsMacOS) {
+    $platform = "darwin"
+} elseif ($IsLinux) {
+    $platform = "linux"
+} else {
+    $platform = "win32"
+}
 
-# Write-Host "Installing Windows SDK..."
-# .\sdksetup.exe /norestart /quiet | Out-Null
+$downloadUrl = (($pythonManifest
+    | Where-Object { $_.version -eq $Version }
+    | Select-Object -First 1).files
+    | Where-Object { $_.platform -eq $platform -and $_.arch -eq $Arch }
+    | Select-Object -First 1).download_url
 
-# Write-Host "Installed Windows SDKs:"
-# Get-ChildItem -Name "${env:ProgramFiles(x86)}\Windows Kits\10\Lib"
+# download
+$tempDir = Join-Path "$HOME_DIR" "python-temp"
+$archive = Join-Path "$tempDir" (Split-Path $downloadUrl -Leaf)
+
+Write-Host "Downloading Python package '$downloadUrl' to '$archive'..."
+New-Item -ItemType Directory -Force -Path "$tempDir" | Out-Null
+(New-Object System.Net.WebClient).DownloadFile("$downloadUrl", "$archive")
+
+# install
+Write-Host "Extracting Python to '$destDir'..."
+New-Item -ItemType Directory -Force -Path "$destDir" | Out-Null
+if ($IsMacOS -or $IsLinux) {
+    tar -vxzf "$archive" -C "$destDir"
+} else {
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$archive", "$destDir")
+}
 
 exit $LASTEXITCODE
