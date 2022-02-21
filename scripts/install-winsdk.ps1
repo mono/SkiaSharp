d169780b4cf84c5b3d94c9ce27d575c8dc04f81f
Matthew Leibowitz <mattleibow@live.com>
2022-02-21 13:21:38 +0800

    Add more checks into things

--- scripts/install-winsdk.ps1
+++ scripts/install-winsdk.ps1
@@ -1,11 +1,19 @@
 Param(
-    [string] $LinkId
+    [string] $LinkId,
+    [string] $Version = ''
 )
 
 $ErrorActionPreference = 'Stop'
 
+$sdkDir = "${env:ProgramFiles(x86)}\Windows Kits\10\Lib"
+
 Write-Host "Installed Windows SDKs:"
-Get-ChildItem -Name "${env:ProgramFiles(x86)}\Windows Kits\10\Lib"
+Get-ChildItem -Name "$sdkDir"
+
+if ($Version -and (Test-Path (Join-Path "$sdkDir" "$Version"))) {
+    Write-Host "Windows SDK $Version already installed."
+    exit 0
+}
 
 $uri = "https://go.microsoft.com/fwlink/p/?LinkId=$LinkId"
 
@@ -16,6 +24,6 @@ Write-Host "Installing Windows SDK..."
 .\sdksetup.exe /norestart /quiet | Out-Null
 
 Write-Host "Installed Windows SDKs:"
-Get-ChildItem -Name "${env:ProgramFiles(x86)}\Windows Kits\10\Lib"
+Get-ChildItem -Name "$sdkDir"
 
 exit $LASTEXITCODE
