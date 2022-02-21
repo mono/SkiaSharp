b9056a5b2a90e05cbe3e1203b802c9fa7898e534
Matthew Leibowitz <mattleibow@live.com>
2022-02-21 14:37:12 +0800

    fgh6tr6f

--- scripts/select-vs.ps1
+++ scripts/select-vs.ps1
@@ -1,7 +1,3 @@
-Param(
-    [string] $Version = "17/pre"
-)
-
 $ErrorActionPreference = 'Stop'
 
 $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
@@ -10,9 +6,7 @@ Write-Host "Setting Environment Variables..."
 $installationPath = & $vswhere -latest -prerelease -property installationPath
 Write-Host "##vso[task.prependpath]$installationPath\MSBuild\Current\Bin"
 Write-Host "##vso[task.setvariable variable=VS_INSTALL]$installationPath"
-Write-Host "##vso[task.setvariable variable=VSINSTALLDIR]$installationPath"
 $env:VS_INSTALL = $installationPath
-$env:VSINSTALLDIR = $installationPath
 
 Write-Host "Installed Visual Studio Versions:"
 & $vswhere -all -prerelease -property installationPath
