910de048cd9ef7d3690f4ad3dfb8fe61ecfd3db0
Matthew Leibowitz <mattleibow@live.com>
2022-02-22 20:39:26 +0800

    Install some Android things

---
+++ scripts/install-android-package.ps1
@@ -0,0 +1,23 @@
+Param(
+    [string] $Package
+)
+
+$ErrorActionPreference = 'Stop'
+
+.\scripts\install-android-sdk.ps1
+$sdk = "$env:ANDROID_SDK_ROOT"
+
+$latest = Join-Path "$sdk" "cmdline-tools" "latest"
+if (-not (Test-Path $latest)) {
+    $versions = Get-ChildItem (Join-Path "$sdk" "cmdline-tools")
+    $latest = ($versions | Select-Object -Last 1)[0]
+}
+
+if (-not $IsMacOS -and -not $IsLinux) {
+    $ext = ".bat"
+}
+$sdkmanager = Join-Path "$latest" "bin" "sdkmanager$ext"
+
+Write-Host "y" | & $sdkmanager "$Package"
+
+exit $LASTEXITCODE
