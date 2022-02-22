02a1035529c65ba379adcdc08b474b6fee145a90
Matthew Leibowitz <mattleibow@live.com>
2022-02-23 02:49:58 +0800

    use correct folder for tests

--- scripts/install-gtk.ps1
+++ scripts/install-gtk.ps1
@@ -9,7 +9,7 @@ $uri = "https://xamarin.azureedge.net/GTKforWindows/Windows/gtk-sharp-$Version.m
 $HOME_DIR = if ($env:HOME) { $env:HOME } else { $env:USERPROFILE }
 $tempDir = Join-Path "$HOME_DIR" "gtk-sharp-temp"
 $installer = Join-Path "$tempDir" "gtk-sharp.msi"
-New-Item -ItemType Directory -Force -Path tempDir | Out-Null
+New-Item -ItemType Directory -Force -Path $tempDir | Out-Null
 
 Write-Host "Downloading GTK# Installer: $uri..."
 .\scripts\download-file.ps1 -Uri $uri -OutFile $installer
--- scripts/install-winsdk.ps1
+++ scripts/install-winsdk.ps1
@@ -5,25 +5,36 @@ Param(
 
 $ErrorActionPreference = 'Stop'
 
-$sdkDir = "${env:ProgramFiles(x86)}\Windows Kits\10\Lib"
+$sdkDir = "${env:ProgramFiles(x86)}\Windows Kits\10\UnionMetadata"
 
 Write-Host "Installed Windows SDKs:"
-Get-ChildItem -Name "$sdkDir"
+if (Test-Path (Join-Path "$sdkDir" "Windows.winmd")) {
+    Write-Host "10.0.10240.0"
+}
+Get-ChildItem -Name "$sdkDir\10.0.*"
 
-if ($Version -and (Test-Path (Join-Path "$sdkDir" "$Version"))) {
+if ($Version -and ((Test-Path (Join-Path "$sdkDir" "$Version")) -or (Test-Path (Join-Path "$sdkDir" "Windows.winmd")))) {
     Write-Host "Windows SDK $Version already installed."
     exit 0
 }
 
 $uri = "https://go.microsoft.com/fwlink/p/?LinkId=$LinkId"
 
+$HOME_DIR = if ($env:HOME) { $env:HOME } else { $env:USERPROFILE }
+$tempDir = Join-Path "$HOME_DIR" "sdksetup-$LinkId-temp"
+$installer = Join-Path "$tempDir" "sdksetup-$LinkId.exe"
+New-Item -ItemType Directory -Force -Path $tempDir | Out-Null
+
 Write-Host "Downloading Windows SDK Installer: $uri..."
-.\scripts\download-file.ps1 -Uri $uri -OutFile sdksetup.exe
+.\scripts\download-file.ps1 -Uri $uri -OutFile $installer
 
 Write-Host "Installing Windows SDK..."
-.\sdksetup.exe /norestart /quiet | Out-Null
+& $installer /norestart /quiet | Out-Null
 
 Write-Host "Installed Windows SDKs:"
-Get-ChildItem -Name "$sdkDir"
+if (Test-Path (Join-Path "$sdkDir" "Windows.winmd")) {
+    Write-Host "10.0.10240.0"
+}
+Get-ChildItem -Name "$sdkDir\10.0.*"
 
 exit $LASTEXITCODE
