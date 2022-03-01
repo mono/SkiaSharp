d2245d459f28d8a9e2b2a2a18300541be7a349f6
Matthew Leibowitz <mattleibow@live.com>
2022-03-01 23:41:08 +0200

    Update install-dotnet-workloads.ps1

--- scripts/install-dotnet-workloads.ps1
+++ scripts/install-dotnet-workloads.ps1
@@ -9,8 +9,12 @@ $ErrorActionPreference = 'Stop'
 $env:DOTNET_ROOT="$InstallDir"
 
 $previewFeed = 'https://api.nuget.org/v3/index.json'
+$previewRuntime = 'https://api.nuget.org/v3/index.json'
+$previewEmscripten = 'https://api.nuget.org/v3/index.json'
 if ($IsPreview) {
   $previewFeed = 'https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json'
+  $previewRuntime = 'https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-runtime-6dd808ff-1/nuget/v3/index.json'
+  $previewEmscripten = 'https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-emsdk-ee0a97a0/nuget/v3/index.json'
 }
 
 Write-Host "Installing workloads..."
@@ -18,6 +22,8 @@ Write-Host "Installing workloads..."
   android ios tvos macos maccatalyst wasm-tools maui `
   --from-rollback-file $SourceUrl `
   --source https://api.nuget.org/v3/index.json `
-  --source $previewFeed
+  --source $previewFeed `
+  --source $previewRuntime `
+  --source $previewEmscripten
 
 exit $LASTEXITCODE
