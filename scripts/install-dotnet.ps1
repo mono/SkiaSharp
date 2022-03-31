e7e5b6495f389b76a42f22ad82963dd41d5c9595
Matthew Leibowitz <mattleibow@live.com>
2022-03-31 08:04:08 +0200

    logging

--- scripts/install-dotnet.ps1
+++ scripts/install-dotnet.ps1
@@ -31,6 +31,7 @@ if (Test-Path $dotnetDll) {
   }
 }
 
+Write-Host "Setting install directry to '$InstallDir'..."
 $env:DOTNET_INSTALL_DIR=$InstallDir
 $env:DOTNET_ROOT=$InstallDir
 $env:DOTNET_MULTILEVEL_LOOKUP=0
@@ -41,6 +42,8 @@ Write-Host "##vso[task.setvariable variable=DOTNET_MULTILEVEL_LOOKUP;]0";
 
 Write-Host "##vso[task.setvariable variable=PATH;]$InstallDir;$env:PATH";
 
+Write-Host "Checking all dotnet info..."
+dir $InstallDir
 & dotnet --info
 
 exit $LASTEXITCODE
