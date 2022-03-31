bb100f9fab1d701db2f8a526ab0ea00cd4f4e930
Matthew Leibowitz <mattleibow@live.com>
2022-03-31 10:05:46 +0200

    quotes

--- scripts/select-xcode.sh
+++ scripts/select-xcode.sh
@@ -2,6 +2,6 @@
 
 XCODE_VERSION=$1
 
-echo '##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]/Applications/Xcode_$XCODE_VERSION.app'
+echo "##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]/Applications/Xcode_$XCODE_VERSION.app"
 
-xcode-select --switch '/Applications/Xcode_$XCODE_VERSION.app/Contents/Developer'
+xcode-select --switch "/Applications/Xcode_$XCODE_VERSION.app/Contents/Developer"
