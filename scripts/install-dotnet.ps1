3895074c79c3813c11700b149de0e66f73cacca3
Matthew Leibowitz <mattleibow@live.com>
2022-03-31 07:51:23 +0200

    always set props

--- scripts/install-dotnet.ps1
+++ scripts/install-dotnet.ps1
@@ -9,27 +9,26 @@ $ErrorActionPreference = 'Stop'
 $dotnetDll = Join-Path "$InstallDir" "sdk" "$Version" "dotnet.dll"
 if (Test-Path $dotnetDll) {
   Write-Host ".NET already installed."
-  exit 0
-}
-
-if ($IsMacOS -or $IsLinux) {
-  $url = "https://dot.net/v1/dotnet-install.sh"
 } else {
-  $url = "https://dot.net/v1/dotnet-install.ps1"
-}
+  if ($IsMacOS -or $IsLinux) {
+    $url = "https://dot.net/v1/dotnet-install.sh"
+  } else {
+    $url = "https://dot.net/v1/dotnet-install.ps1"
+  }
 
-Write-Host "Downloading .NET Installer..."
-Invoke-WebRequest `
-  -Uri "$url" `
-  -OutFile (Split-Path $url -Leaf)
+  Write-Host "Downloading .NET Installer..."
+  Invoke-WebRequest `
+    -Uri "$url" `
+    -OutFile (Split-Path $url -Leaf)
 
-Write-Host "Installing .NET $Version..."
-if ($IsMacOS) {
-  & sh dotnet-install.sh --version "$Version" --install-dir "$InstallDir" --azure-feed "$FeedUrl" --verbose
-} elseif ($IsLinux) {
-  & bash dotnet-install.sh --version "$Version" --install-dir "$InstallDir" --azure-feed "$FeedUrl" --verbose
-} else {
-  .\dotnet-install.ps1 -Version "$Version" -InstallDir "$InstallDir" -AzureFeed "$FeedUrl" -Verbose
+  Write-Host "Installing .NET $Version..."
+  if ($IsMacOS) {
+    & sh dotnet-install.sh --version "$Version" --install-dir "$InstallDir" --azure-feed "$FeedUrl" --verbose
+  } elseif ($IsLinux) {
+    & bash dotnet-install.sh --version "$Version" --install-dir "$InstallDir" --azure-feed "$FeedUrl" --verbose
+  } else {
+    .\dotnet-install.ps1 -Version "$Version" -InstallDir "$InstallDir" -AzureFeed "$FeedUrl" -Verbose
+  }
 }
 
 $env:DOTNET_INSTALL_DIR=$InstallDir
