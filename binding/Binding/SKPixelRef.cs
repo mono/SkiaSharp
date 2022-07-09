9ede35550d42d98da0c4cfb9099a72a659da4e2b
mgood7123 <smallville7123@gmail.com>
2022-07-09 20:07:11 +1000

    rename to match

--- binding/Binding/SKPixelRef.cs
+++ binding/Binding/SKPixelRef.cs
@@ -58,7 +58,7 @@ namespace SkiaSharp
 			}
 		}
 
-		[MonoPInvokeCallback(typeof(SKPixelrefDestroyProxyDelegate))]
+		[MonoPInvokeCallback(typeof(SKPixelRefDestroyProxyDelegate))]
 		private static void DestroyInternal(IntPtr s, void* context)
 		{
 			var id = DelegateProxies.GetUserData<SKPixelRef>((IntPtr)context, out var gch);
