5816e44dcd6067ec7e6993acb1540182b681a2d0
Matthew Leibowitz <mattleibow@live.com>
2022-10-03 09:53:56 +0800

    Intercept and capture GL for .NET (#2268)

---
+++ source/SkiaSharp.Views.Blazor/SkiaSharp.Views.Blazor/nuget/build/net6.0/SkiaSharpGLInterop.js
@@ -0,0 +1,15 @@
+﻿// Workaround for https://github.com/dotnet/runtime/issues/76077
+// Special thanks to the Avalonia UI team
+
+var SkiaSharpGLInterop = {
+	$SkiaSharpLibrary: {
+		internal_func: function () {
+		}
+	},
+	InterceptGLObject: function () {
+		globalThis.SkiaSharpGL = GL
+	}
+}
+
+autoAddDeps(SkiaSharpGLInterop, '$SkiaSharpLibrary')
+mergeInto(LibraryManager.library, SkiaSharpGLInterop)
