bcb94c843d340dcd83f6122fd18cdffd96c113ff
Matthew Leibowitz <mattleibow@live.com>
2023-06-24 01:20:25 +0200

    harfbuzzand more

--- binding/HarfBuzzSharp/Properties/HarfBuzzSharpAssemblyInfo.cs
+++ binding/HarfBuzzSharp/Properties/HarfBuzzSharpAssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: InternalsVisibleTo ("SkiaSharp.Tests, PublicKey=" +
+[assembly: InternalsVisibleTo ("HarfBuzzSharp.Tests, PublicKey=" +
 	"002400000480000094000000060200000024000052534131000400000100010079159977d2d03a" +
 	"8e6bea7a2e74e8d1afcc93e8851974952bb480a12c9134474d04062447c37e0e68c080536fcf3c" +
 	"3fbe2ff9c979ce998475e506e8ce82dd5b0f350dc10e93bf2eeecf874b24770c5081dbea7447fd" +
