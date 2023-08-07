04cc2260e167b4295cc41be6a182b86538075bdf
Matthew Leibowitz <mattleibow@live.com>
2023-08-07 23:14:35 +0200

    Update the samples

--- samples/Basic/Console/SkiaSharpSample/Program.cs
+++ samples/Basic/Console/SkiaSharpSample/Program.cs
@@ -14,34 +14,33 @@ namespace SkiaSharpSample
 
 			// crate a surface
 			var info = new SKImageInfo(256, 256);
-			using (var surface = SKSurface.Create(info))
+			using var surface = SKSurface.Create(info);
+
+			// the the canvas and properties
+			var canvas = surface.Canvas;
+
+			// make sure the canvas is blank
+			canvas.Clear(SKColors.White);
+
+			// draw some text
+			using var paint = new SKPaint
 			{
-				// the the canvas and properties
-				var canvas = surface.Canvas;
+				Color = SKColors.Black,
+				IsAntialias = true,
+				Style = SKPaintStyle.Fill
+			};
+			using var font = new SKFont
+			{
+				Size = 24
+			};
+			var coord = new SKPoint(info.Width / 2, (info.Height + font.Size) / 2);
+			canvas.DrawText("SkiaSharp", coord, SKTextAlign.Center, font, paint);
 
-				// make sure the canvas is blank
-				canvas.Clear(SKColors.White);
-
-				// draw some text
-				var paint = new SKPaint
-				{
-					Color = SKColors.Black,
-					IsAntialias = true,
-					Style = SKPaintStyle.Fill,
-					TextAlign = SKTextAlign.Center,
-					TextSize = 24
-				};
-				var coord = new SKPoint(info.Width / 2, (info.Height + paint.TextSize) / 2);
-				canvas.DrawText("SkiaSharp", coord, paint);
-
-				// save the file
-				using (var image = surface.Snapshot())
-				using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
-				using (var stream = File.OpenWrite("output.png"))
-				{
-					data.SaveTo(stream);
-				}
-			}
+			// save the file
+			using var image = surface.Snapshot();
+			using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+			using var stream = File.OpenWrite("output.png");
+			data.SaveTo(stream);
 		}
 	}
 }
--- samples/Basic/Web/SkiaSharpSample/Controllers/Api/ImagesController.cs
+++ samples/Basic/Web/SkiaSharpSample/Controllers/Api/ImagesController.cs
@@ -30,32 +30,31 @@ namespace SkiaSharpSample.Controllers
 		{
 			// create a surface
 			var info = new SKImageInfo(512, 512);
-			using (var surface = SKSurface.Create(info))
+			using var surface = SKSurface.Create(info);
+
+			// the the canvas and properties
+			var canvas = surface.Canvas;
+
+			// make sure the canvas is blank
+			canvas.Clear(SKColors.White);
+
+			// draw some text
+			using var paint = new SKPaint
 			{
-				// the the canvas and properties
-				var canvas = surface.Canvas;
+				Color = SKColors.Black,
+				IsAntialias = true,
+				Style = SKPaintStyle.Fill
+			};
+			using var font = new SKFont
+			{
+				Size = 24
+			};
+			var coord = new SKPoint(info.Width / 2, (info.Height + font.Size) / 2);
+			canvas.DrawText(text, coord, SKTextAlign.Center, font, paint);
 
-				// make sure the canvas is blank
-				canvas.Clear(SKColors.White);
-
-				// draw some text
-				var paint = new SKPaint
-				{
-					Color = SKColors.Black,
-					IsAntialias = true,
-					Style = SKPaintStyle.Fill,
-					TextAlign = SKTextAlign.Center,
-					TextSize = 48
-				};
-				var coord = new SKPoint(info.Width / 2, (info.Height + paint.TextSize) / 2);
-				canvas.DrawText(text, coord, paint);
-
-				// retrieve the encoded image
-				using (var image = surface.Snapshot())
-				{
-					return image.Encode(SKEncodedImageFormat.Png, 100);
-				}
-			}
+			// retrieve the encoded image
+			using var image = surface.Snapshot();
+			return image.Encode(SKEncodedImageFormat.Png, 100);
 		}
 	}
 }
