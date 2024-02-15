a0ff234638b607ca4b3a4bb09916282a3e9e6a8c
Max Katz <maxkatz6@outlook.com>
2024-02-15 00:37:48 -0800

    Revert "Make SKRuntimeEffect slightly more compatible with 3.0"
    This reverts commit 19c3b6756145bf487d60985e0b8af013e5c91877.

--- binding/Binding/SKRuntimeEffect.cs
+++ binding/Binding/SKRuntimeEffect.cs
@@ -17,10 +17,7 @@ namespace SkiaSharp
 
 		// Create
 
-		[Obsolete("Use SKRuntimeEffect.CreateShader() instead.")]
-		public static SKRuntimeEffect Create (string sksl, out string errors) => CreateShader (sksl, out errors);
-
-		public static SKRuntimeEffect CreateShader (string sksl, out string errors)
+		public static SKRuntimeEffect Create (string sksl, out string errors)
 		{
 			using var s = new SKString (sksl);
 			using var errorString = new SKString ();
@@ -66,26 +63,14 @@ namespace SkiaSharp
 
 		// ToShader
 
-		public SKShader ToShader () =>
-			ToShader (true, null, null, null);
-
 		public SKShader ToShader (bool isOpaque) =>
 			ToShader (isOpaque, null, null, null);
-
-		public SKShader ToShader (SKRuntimeEffectUniforms uniforms) =>
-			ToShader (true, uniforms.ToData (), null, null);
 
 		public SKShader ToShader (bool isOpaque, SKRuntimeEffectUniforms uniforms) =>
 			ToShader (isOpaque, uniforms.ToData (), null, null);
 
-		public SKShader ToShader (SKRuntimeEffectUniforms uniforms, SKRuntimeEffectChildren children) =>
-			ToShader (true, uniforms.ToData (), children.ToArray (), null);
-
 		public SKShader ToShader (bool isOpaque, SKRuntimeEffectUniforms uniforms, SKRuntimeEffectChildren children) =>
 			ToShader (isOpaque, uniforms.ToData (), children.ToArray (), null);
-
-		public SKShader ToShader (SKRuntimeEffectUniforms uniforms, SKRuntimeEffectChildren children, SKMatrix localMatrix) =>
-			ToShader (true, uniforms.ToData (), children.ToArray (), &localMatrix);
 
 		public SKShader ToShader (bool isOpaque, SKRuntimeEffectUniforms uniforms, SKRuntimeEffectChildren children, SKMatrix localMatrix) =>
 			ToShader (isOpaque, uniforms.ToData (), children.ToArray (), &localMatrix);
