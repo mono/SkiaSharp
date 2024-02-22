6751f0cb26b06ab3d2ba496575440eeafbfb578e
gmurray81 <gmurray@gmail.com>
2024-02-21 23:23:33 -0500

    simplify releasing the managed resources

--- source/SkiaSharp.Views/SkiaSharp.Views.WPF/SKGLElement.cs
+++ source/SkiaSharp.Views/SkiaSharp.Views.WPF/SKGLElement.cs
@@ -40,7 +40,6 @@ namespace SkiaSharp.Views.WPF
 		private SKCanvas canvas;
 
 		private SKSizeI lastSize;
-		private SKGLElementWindowListener listener;
 
 		public SKGLElement()
 			: base()
@@ -50,16 +49,13 @@ namespace SkiaSharp.Views.WPF
 
 		private void Initialize()
 		{
-            designMode = DesignerProperties.GetIsInDesignMode(this);
-            var settings = new GLWpfControlSettings() { MajorVersion = 2, MinorVersion = 1, RenderContinuously = false };
-			
-            this.Render += OnPaint;
+			designMode = DesignerProperties.GetIsInDesignMode(this);
+			var settings = new GLWpfControlSettings() { MajorVersion = 2, MinorVersion = 1, RenderContinuously = false };
 
-            this.Loaded += (s, e) =>
-            {
-				SKGLElementWindowListener listener = new SKGLElementWindowListener(this);
-				this.listener = listener;
-            };
+			this.Render += OnPaint;
+
+			this.Loaded += SKGLElement_Loaded;
+			this.Unloaded += SKGLElement_Unloaded;
 
 #if NETCOREAPP
 			this.RegisterToEventsDirectly = false;
@@ -68,64 +64,14 @@ namespace SkiaSharp.Views.WPF
 			Start(settings);
 		}
 
-
-		private class SKGLElementWindowListener: IDisposable
-        {
-            private WeakReference<SKGLElement> toDestroy;
-			private WeakReference<Window> theWindow;
-			public SKGLElementWindowListener(SKGLElement toDestroy)
-            {
-                this.toDestroy = new WeakReference<SKGLElement>(toDestroy);
-				var window = System.Windows.Window.GetWindow(toDestroy);
-				if (window != null)
-				{
-					theWindow = new WeakReference<Window>(window);
-					window.Closing += Window_Closing;
-				}
-            }
-
-            private void Window_Closing(object sender, CancelEventArgs e)
-            {
-                SKGLElement target = null;
-                if (toDestroy.TryGetTarget(out target))
-                {
-                    if (target != null)
-                    {
-                        target.Dispose();
-                    }
-                }
-
-            }
-
-			private bool disposed = false;
-
-
-			protected virtual void Dispose(bool disposing)
-			{
-				if (disposed)
-				{
-					return;
-				}
-
-				Window window;
-				if (theWindow != null && theWindow.TryGetTarget(out window))
-				{
-					if (window != null)
-					{
-						window.Closing -= Window_Closing;
-					}
-				}
-				theWindow = null;
-
-				disposed = true;
-			}
-
-			public void Dispose()
-			{
-				Dispose(true);
-			}
+		private void SKGLElement_Unloaded(object sender, RoutedEventArgs e)
+		{
+			Release();
 		}
-
+		private void SKGLElement_Loaded(object sender, RoutedEventArgs e)
+		{
+			InvalidateVisual();
+		}
 
 		public SKSize CanvasSize => lastSize;
 
@@ -134,50 +80,50 @@ namespace SkiaSharp.Views.WPF
 		[Category("Appearance")]
 		public event EventHandler<SKPaintGLSurfaceEventArgs> PaintSurface;
 
-        private SKSizeI GetSize()
+		private SKSizeI GetSize()
 		{
-            var currentWidth = ActualWidth;
-            var currentHeight = ActualHeight;
+			var currentWidth = ActualWidth;
+			var currentHeight = ActualHeight;
 
-            if (currentWidth < 0 ||
-                currentHeight < 0)
+			if (currentWidth < 0 ||
+				currentHeight < 0)
 			{
-                currentWidth = 0;
-                currentHeight = 0;
-            }
-
-            PresentationSource source = PresentationSource.FromVisual(this);
-
-            double dpiX = 1.0;
-            double dpiY = 1.0;
-            if (source != null)
-            {
-                dpiX = source.CompositionTarget.TransformToDevice.M11;
-                dpiY = source.CompositionTarget.TransformToDevice.M22;
+				currentWidth = 0;
+				currentHeight = 0;
 			}
 
-            return new SKSizeI((int)(currentWidth * dpiX), (int)(currentHeight * dpiY));
-        }
+			PresentationSource source = PresentationSource.FromVisual(this);
 
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            if (grContext != null)
-            {
-                grContext.ResetContext();
-            }
-            base.OnRender(drawingContext);
-        }
+			double dpiX = 1.0;
+			double dpiY = 1.0;
+			if (source != null)
+			{
+				dpiX = source.CompositionTarget.TransformToDevice.M11;
+				dpiY = source.CompositionTarget.TransformToDevice.M22;
+			}
 
-        protected virtual void OnPaint(TimeSpan e)
-        {
-            if (disposed)
-            {
-                return;
-            }
-            if (designMode)
-            {
-                return;
-            }
+			return new SKSizeI((int)(currentWidth * dpiX), (int)(currentHeight * dpiY));
+		}
+
+		protected override void OnRender(DrawingContext drawingContext)
+		{
+			if (grContext != null)
+			{
+				grContext.ResetContext();
+			}
+			base.OnRender(drawingContext);
+		}
+
+		protected virtual void OnPaint(TimeSpan e)
+		{
+			if (disposed)
+			{
+				return;
+			}
+			if (designMode)
+			{
+				return;
+			}
 
 			// create the contexts if not done already
 			if (grContext == null)
@@ -187,11 +133,11 @@ namespace SkiaSharp.Views.WPF
 			}
 
 			// get the new surface size
-            var newSize = GetSize();
+			var newSize = GetSize();
 
-            GL.ClearColor(Color4.Transparent);
-            GL.Clear(ClearBufferMask.ColorBufferBit);
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
+			GL.ClearColor(Color4.Transparent);
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
 
 			// manage the drawing surface
 			if (renderTarget == null || lastSize != newSize || !renderTarget.IsValid)
@@ -201,7 +147,7 @@ namespace SkiaSharp.Views.WPF
 				lastSize = newSize;
 
 				GL.GetInteger(GetPName.FramebufferBinding, out var framebuffer);
-                GL.GetInteger(GetPName.StencilBits, out var stencil);
+				GL.GetInteger(GetPName.StencilBits, out var stencil);
 				GL.GetInteger(GetPName.Samples, out var samples);
 				var maxSamples = grContext.GetMaxSurfaceSampleCount(colorType);
 				if (samples > maxSamples)
@@ -241,17 +187,23 @@ namespace SkiaSharp.Views.WPF
 			PaintSurface?.Invoke(this, e);
 		}
 
-        private bool disposed = false;
+		private bool disposed = false;
 
 
-        protected virtual void Dispose(bool disposing)
-        {
-		    if (disposed)
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposed)
 			{
-                return;
-            }
+				return;
+			}
 
-			
+			Release();
+
+			disposed = true;
+		}
+
+		private void Release()
+		{
 			canvas = null;
 			surface?.Dispose();
 			surface = null;
@@ -259,13 +211,9 @@ namespace SkiaSharp.Views.WPF
 			renderTarget = null;
 			grContext?.Dispose();
 			grContext = null;
-			listener?.Dispose();
-			listener = null;
+		}
 
-			disposed = true;
-        }
-
-        public void Dispose()
+		public void Dispose()
         {
             Dispose(true);
 		}
