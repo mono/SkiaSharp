a9bec2a01e82f37c959f50fb8c5c161e35837a5d
Kation <kation@outlook.com>
2024-04-12 03:25:46 +0800

    vortice warpper

--- source/SkiaSharp.Direct3D/SkiaSharp.Direct3D.Vortice/GRDirect3DBackendContext.cs
+++ source/SkiaSharp.Direct3D/SkiaSharp.Direct3D.Vortice/GRDirect3DBackendContext.cs
@@ -11,12 +11,12 @@ namespace SkiaSharp
 	public class GRDirect3DBackendContext : GRD3DBackendContext
 	{
 		private IDXGIAdapter1 _adapter;
-		public new IDXGIAdapter1 Adapter { get => _adapter; set { _adapter = value; base.Resource = value?.NativePointer ?? default; } }
+		public new IDXGIAdapter1 Adapter { get => _adapter; set { _adapter = value; base.Adapter = value?.NativePointer ?? default; } }
 
 		private ID3D12Device2 _device;
 		public new ID3D12Device2 Device { get => _device; set { _device = value; base.Device = value?.NativePointer ?? default; } }
 
 		private ID3D12CommandQueue _queue;
-		public new ID3D12CommandQueue Queue { get => _queue; set { _device = value; base.Queue = value?.NativePointer ?? default; } }
+		public new ID3D12CommandQueue Queue { get => _queue; set { _queue = value; base.Queue = value?.NativePointer ?? default; } }
 	}
 }
