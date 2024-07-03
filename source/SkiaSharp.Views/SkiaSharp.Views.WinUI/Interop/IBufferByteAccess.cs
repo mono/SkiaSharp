3715a99bd9632e463201ce72f063eb75ea226bd6
Matthew Leibowitz <mattleibow@live.com>
2024-07-03 23:30:28 +0200

    Update IBufferByteAccess for AOT
    Fixes https://github.com/microsoft/CsWinRT/issues/1645

--- source/SkiaSharp.Views/SkiaSharp.Views.WinUI/Interop/IBufferByteAccess.cs
+++ source/SkiaSharp.Views/SkiaSharp.Views.WinUI/Interop/IBufferByteAccess.cs
@@ -8,6 +8,7 @@ namespace SkiaSharp.Views.Windows
 {
 	[WindowsRuntimeType("Windows.Foundation.UniversalApiContract")]
 	[Guid("905a0fef-bc53-11df-8c49-001e4fc686da")]
+	[WindowsRuntimeHelperType(typeof(ABI.SkiaSharp.Views.Windows))]
 	internal interface IBufferByteAccess
 	{
 		IntPtr Buffer { get; }
@@ -30,51 +31,33 @@ namespace ABI.SkiaSharp.Views.Windows
 {
 	[DynamicInterfaceCastableImplementation]
 	[Guid("905a0fef-bc53-11df-8c49-001e4fc686da")]
-	internal interface IBufferByteAccess : global::SkiaSharp.Views.Windows.IBufferByteAccess
+	internal unsafe interface IBufferByteAccess : global::SkiaSharp.Views.Windows.IBufferByteAccess
 	{
-		public struct VftblPtr
-		{
-			public IntPtr Vftbl;
-		}
-
 		[Guid("905a0fef-bc53-11df-8c49-001e4fc686da")]
 		public struct Vftbl
 		{
-			public delegate int GetBufferDelegate(IntPtr thisPtr, out IntPtr buffer);
-
 			internal IUnknownVftbl IUnknownVftbl;
-			public GetBufferDelegate getBuffer;
+			public delegate* unmanaged[Stdcall]<IntPtr, IntPtr*, int> getBuffer;
+
+			public static readonly IntPtr AbiToProjectionVftablePtr;
 
 			static unsafe Vftbl()
 			{
-				AbiToProjectionVftable = new Vftbl
+				AbiToProjectionVftablePtr = ComWrappersSupport.AllocateVtableMemory(typeof(Vftbl), sizeof(IUnknownVftbl) + sizeof(IntPtr));
+				(*(Vftbl*)AbiToProjectionVftablePtr) = new Vftbl
 				{
 					IUnknownVftbl = IUnknownVftbl.AbiToProjectionVftbl,
-					getBuffer = DoAbiGetBuffer
+					getBuffer = &DoAbiGetBuffer,
 				};
-				var nativeVftbl = (IntPtr*)Marshal.AllocCoTaskMem(Marshal.SizeOf<IUnknownVftbl>() + sizeof(IntPtr) * 12);
-				Marshal.StructureToPtr(AbiToProjectionVftable.IUnknownVftbl, (IntPtr)nativeVftbl, false);
-				nativeVftbl[3] = Marshal.GetFunctionPointerForDelegate(AbiToProjectionVftable.getBuffer);
-				AbiToProjectionVftablePtr = (IntPtr)nativeVftbl;
 			}
 
-			public static readonly Vftbl AbiToProjectionVftable;
-			public static readonly IntPtr AbiToProjectionVftablePtr;
-
-			internal unsafe Vftbl(IntPtr ptr)
+			[UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
+			private static int DoAbiGetBuffer(IntPtr thisPtr, IntPtr* buffer)
 			{
-				var vftblPtr = Marshal.PtrToStructure<VftblPtr>(ptr);
-				var vftbl = (IntPtr*)vftblPtr.Vftbl;
-				IUnknownVftbl = Marshal.PtrToStructure<IUnknownVftbl>(vftblPtr.Vftbl);
-				getBuffer = Marshal.GetDelegateForFunctionPointer<GetBufferDelegate>(vftbl[3]);
-			}
-
-			private static int DoAbiGetBuffer(IntPtr thisPtr, out IntPtr buffer)
-			{
-				buffer = default;
+				*buffer = default;
 				try
 				{
-					buffer = ComWrappersSupport.FindObject<global::SkiaSharp.Views.Windows.IBufferByteAccess>(thisPtr).Buffer;
+					*buffer = ComWrappersSupport.FindObject<global::SkiaSharp.Views.Windows.IBufferByteAccess>(thisPtr).Buffer;
 				}
 				catch (Exception ex)
 				{
@@ -83,19 +66,18 @@ namespace ABI.SkiaSharp.Views.Windows
 				return 0;
 			}
 		}
+		internal static ObjectReference<IUnknownVftbl> FromAbi(IntPtr thisPtr) => ObjectReference<IUnknownVftbl>.FromAbi(thisPtr, IID.IID_IBufferByteAccess);
 
 		IntPtr global::SkiaSharp.Views.Windows.IBufferByteAccess.Buffer
 		{
 			get
 			{
-				var obj = (ObjectReference<Vftbl>)((IWinRTObject)this).GetObjectReferenceForType(typeof(global::SkiaSharp.Views.Windows.IBufferByteAccess).TypeHandle);
+				var obj = ((IWinRTObject)this).GetObjectReferenceForType(typeof(global::SkiaSharp.Views.Windows.IBufferByteAccess).TypeHandle);
 				var ThisPtr = obj.ThisPtr;
-				Marshal.ThrowExceptionForHR(obj.Vftbl.getBuffer(ThisPtr, out IntPtr buffer));
+				IntPtr buffer = default;
+				Marshal.ThrowExceptionForHR(((delegate* unmanaged[Stdcall]<IntPtr, IntPtr*, int>)(*(void***)ThisPtr)[3])(ThisPtr, &buffer));
 				return buffer;
 			}
 		}
-
-		internal static ObjectReference<Vftbl> FromAbi(IntPtr thisPtr) =>
-			ObjectReference<Vftbl>.FromAbi(thisPtr);
 	}
 }
