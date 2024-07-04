e7ac159da9bd432307cf3fe9e190dc9488bac907
Matthew Leibowitz <mattleibow@live.com>
2024-07-05 03:54:43 +0800

    Just use the native library

--- source/SkiaSharp.Views/SkiaSharp.Views.WinUI/UWPExtensions.cs
+++ source/SkiaSharp.Views/SkiaSharp.Views.WinUI/UWPExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Windows.Foundation;
 using Windows.UI;
+using Windows.Storage.Streams;
+using SkiaSharp.Views.WinUI.Native;
+
 
 #if WINDOWS || WINUI
 using Microsoft.UI.Xaml.Media.Imaging;
@@ -171,6 +174,9 @@ namespace SkiaSharp.Views.UWP
 
 		internal static IntPtr GetPixels(this WriteableBitmap bitmap) =>
 			bitmap.PixelBuffer.GetByteBuffer();
+
+		internal static IntPtr GetByteBuffer(this IBuffer buffer) =>
+			(IntPtr)BufferExtensions.GetByteBuffer(buffer);
 #endif
 	}
 }
