3667eee221f050fc460725910f7270bb1ec07f98
Matthew Leibowitz <mattleibow@live.com>
2024-07-27 23:25:16 +0800

    Some VMs need a new SDK

--- scripts/install-android-sdk.ps1
+++ scripts/install-android-sdk.ps1
@@ -1,32 +1,35 @@
+Param(
+    [string] $InstallDestination = $null
+)
+
 $ErrorActionPreference = 'Stop'
 
-if ("$env:ANDROID_HOME" -and "$env:ANDROID_SDK_ROOT") {
-    if ("$env:ANDROID_HOME" -eq "$env:ANDROID_SDK_ROOT") {
-        Write-Host "Android SDK is already installed at $env:ANDROID_HOME."
-        Write-Host "Android SDK contents:"
-        Get-ChildItem "$env:ANDROID_HOME"
-        exit 0
-    } else {
-        Write-Warning "Android SDK environment variables were different:"
-        Write-Warning "ANDROID_SDK_ROOT=$env:ANDROID_SDK_ROOT."
-        Write-Warning "ANDROID_HOME=$env:ANDROID_HOME."
-        Write-Host "ANDROID_SDK_ROOT Android SDK contents:"
-        Get-ChildItem "$env:ANDROID_SDK_ROOT"
-        Write-Host "ANDROID_HOME Android SDK contents:"
-        Get-ChildItem "$env:ANDROID_HOME"
-        exit 1
-    }
-}
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$HOME_DIR = if ($env:HOME) { $env:HOME } else { $env:USERPROFILE }
 
 Write-Host "Looking for an existing Android SDK..."
 
 # try use some environment variables
 if ("$env:ANDROID_SDK_ROOT") {
-    Write-Host "Found environment variable ANDROID_SDK_ROOT=$env:ANDROID_SDK_ROOT."
-    $sdk = "$env:ANDROID_SDK_ROOT"
-} elseif ("$env:ANDROID_HOME") {
-    Write-Host "Found environment variable ANDROID_HOME=$env:ANDROID_HOME."
-    $sdk = "$env:ANDROID_HOME"
+    Write-Host "Found environment variable ANDROID_SDK_ROOT=$env:ANDROID_SDK_ROOT, checking for command line tools..."
+    $latest = Join-Path "$env:ANDROID_SDK_ROOT" "cmdline-tools" "latest"
+    if (Test-Path $latest) {
+        $sdk = "$env:ANDROID_SDK_ROOT"
+        Write-Host "Found command line tools in $latest."
+    } else {
+        Write-Host "No command line tools found, not going to use this one."
+    }
+}
+if (-not $sdk -and "$env:ANDROID_HOME") {
+    Write-Host "Found environment variable ANDROID_HOME=$env:ANDROID_HOME, checking for command line tools..."
+    $latest = Join-Path "$env:ANDROID_HOME" "cmdline-tools" "latest"
+    if (Test-Path $latest) {
+        $sdk = "$env:ANDROID_HOME"
+        Write-Host "Found command line tools in $latest."
+    } else {
+        Write-Host "No command line tools found, not going to use this one."
+    }
 }
 
 # look in program files from VS
@@ -40,10 +43,56 @@ if (-not $sdk -and -not $IsMacOS -and -not $IsLinux) {
     }
 }
 
-# for now just thro as most machines have the SDK somewhere
+# Nothing was found, so we need to install the SDK
 if (-not $sdk) {
-    Write-Error "TODO: actually install the Android SDK"
-    exit 1
+    # detect
+    Write-Host "Getting the latest command line tool info..."
+    $repoUrl = "https://dl.google.com/android/repository/repository2-1.xml"
+    [xml] $repoXml = Invoke-WebRequest -Uri $repoUrl
+    $stable = $repoXml.DocumentElement.remotePackage | Where-Object { $_.path -eq "cmdline-tools;latest" -and $_.channelRef.ref -eq "channel-0" }
+    $platform = if ($IsMacOS) { "macosx" } elseif ($IsLinux) { "linux" } else { "windows" }
+    $archive = $stable.archives.archive | Where-Object { $_."host-os" -eq $platform }
+    $filename = $archive.complete.url
+    Write-Host "Latest command line tools are $filename..."
+    $url = "https://dl.google.com/android/repository/$filename"
+
+    $sdk = "$HOME_DIR/android-sdk"
+    if ($InstallDestination) {
+        $sdk = $InstallDestination
+    }
+    Write-Host "Install destination is '$sdk'."
+
+    $sdkTemp = "$HOME_DIR/android-sdk-temp"
+    $zip = "$sdkTemp/android-sdk-cmdline-tools.zip"
+
+    # download
+    # if (-not (Test-Path $zip)) {
+    Write-Host "Downloading SDK ($url) to '$zip'..."
+    New-Item -ItemType Directory -Force -Path "$sdkTemp" | Out-Null
+    (New-Object System.Net.WebClient).DownloadFile("$url", "$zip")
+    # }
+
+    # extract
+    if (Test-Path "$sdkTemp/extracted") {
+        Write-Host "Removing old extracted SDK from $sdkTemp/extracted..."
+        Remove-Item -Recurse -Force "$sdkTemp/extracted"
+    }
+    if (Test-Path "$sdk") {
+        Write-Host "Removing old SDK from $sdk..."
+        Remove-Item -Recurse -Force "$sdk"
+    }
+    Write-Host "Extracting SDK to $sdkTemp/extracted..."
+    if ($IsLinux -or $IsMacOS) {
+        unzip "$zip" -d "$sdkTemp/extracted"
+        Write-Host "Moving SDK..."
+        New-Item -ItemType Directory -Force -Path "$sdk/cmdline-tools/latest" | Out-Null
+        Move-Item "${sdkTemp}/extracted/cmdline-tools/*" "$sdk/cmdline-tools/latest/"
+    } else {
+        [System.IO.Compression.ZipFile]::ExtractToDirectory("$zip", "$sdkTemp/extracted")
+        Write-Host "Moving SDK..."
+        New-Item -ItemType Directory -Force -Path "$sdk/cmdline-tools/latest" | Out-Null
+        Move-Item "${sdkTemp}/extracted/cmdline-tools/*" "$sdk/cmdline-tools/latest/"
+    }
 }
 
 # make sure that the SDK is in ANDROID_HOME and ANDROID_SDK_ROOT
@@ -53,8 +102,5 @@ Write-Host "##vso[task.setvariable variable=ANDROID_SDK_ROOT;]$sdk";
 Write-Host "##vso[task.setvariable variable=ANDROID_HOME;]$sdk";
 $env:ANDROID_SDK_ROOT = $sdk
 $env:ANDROID_HOME = $sdk
-
-Write-Host "Android SDK contents:"
-Get-ChildItem "$sdk"
 
 exit $LASTEXITCODE
