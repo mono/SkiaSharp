3a6c55e6d2ea3e6135b48ee7692c5dc699b942e6
Matthew Leibowitz <mattleibow@live.com>
2025-03-01 20:25:04 +0800

    regen

--- binding/SkiaSharp/SkiaApi.generated.cs
+++ binding/SkiaSharp/SkiaApi.generated.cs
@@ -18449,6 +18449,63 @@ namespace SkiaSharp {
 
 	}
 
+	// sk_document_pdf_datetime_t
+	[StructLayout (LayoutKind.Sequential)]
+	internal unsafe partial struct SKTimeDateTimeInternal : IEquatable<SKTimeDateTimeInternal> {
+		// public int16_t fTimeZoneMinutes
+		public Int16 fTimeZoneMinutes;
+
+		// public uint16_t fYear
+		public UInt16 fYear;
+
+		// public uint8_t fMonth
+		public Byte fMonth;
+
+		// public uint8_t fDayOfWeek
+		public Byte fDayOfWeek;
+
+		// public uint8_t fDay
+		public Byte fDay;
+
+		// public uint8_t fHour
+		public Byte fHour;
+
+		// public uint8_t fMinute
+		public Byte fMinute;
+
+		// public uint8_t fSecond
+		public Byte fSecond;
+
+		public readonly bool Equals (SKTimeDateTimeInternal obj) =>
+#pragma warning disable CS8909
+			fTimeZoneMinutes == obj.fTimeZoneMinutes && fYear == obj.fYear && fMonth == obj.fMonth && fDayOfWeek == obj.fDayOfWeek && fDay == obj.fDay && fHour == obj.fHour && fMinute == obj.fMinute && fSecond == obj.fSecond;
+#pragma warning restore CS8909
+
+		public readonly override bool Equals (object obj) =>
+			obj is SKTimeDateTimeInternal f && Equals (f);
+
+		public static bool operator == (SKTimeDateTimeInternal left, SKTimeDateTimeInternal right) =>
+			left.Equals (right);
+
+		public static bool operator != (SKTimeDateTimeInternal left, SKTimeDateTimeInternal right) =>
+			!left.Equals (right);
+
+		public readonly override int GetHashCode ()
+		{
+			var hash = new HashCode ();
+			hash.Add (fTimeZoneMinutes);
+			hash.Add (fYear);
+			hash.Add (fMonth);
+			hash.Add (fDayOfWeek);
+			hash.Add (fDay);
+			hash.Add (fHour);
+			hash.Add (fMinute);
+			hash.Add (fSecond);
+			return hash.ToHashCode ();
+		}
+
+	}
+
 	// sk_document_pdf_metadata_t
 	[StructLayout (LayoutKind.Sequential)]
 	internal unsafe partial struct SKDocumentPdfMetadataInternal : IEquatable<SKDocumentPdfMetadataInternal> {
@@ -18470,10 +18527,10 @@ namespace SkiaSharp {
 		// public sk_string_t* fProducer
 		public sk_string_t fProducer;
 
-		// public sk_time_datetime_t* fCreation
+		// public sk_document_pdf_datetime_t* fCreation
 		public SKTimeDateTimeInternal* fCreation;
 
-		// public sk_time_datetime_t* fModified
+		// public sk_document_pdf_datetime_t* fModified
 		public SKTimeDateTimeInternal* fModified;
 
 		// public float fRasterDPI
@@ -19955,63 +20012,6 @@ namespace SkiaSharp {
 
 	}
 
-	// sk_time_datetime_t
-	[StructLayout (LayoutKind.Sequential)]
-	internal unsafe partial struct SKTimeDateTimeInternal : IEquatable<SKTimeDateTimeInternal> {
-		// public int16_t fTimeZoneMinutes
-		public Int16 fTimeZoneMinutes;
-
-		// public uint16_t fYear
-		public UInt16 fYear;
-
-		// public uint8_t fMonth
-		public Byte fMonth;
-
-		// public uint8_t fDayOfWeek
-		public Byte fDayOfWeek;
-
-		// public uint8_t fDay
-		public Byte fDay;
-
-		// public uint8_t fHour
-		public Byte fHour;
-
-		// public uint8_t fMinute
-		public Byte fMinute;
-
-		// public uint8_t fSecond
-		public Byte fSecond;
-
-		public readonly bool Equals (SKTimeDateTimeInternal obj) =>
-#pragma warning disable CS8909
-			fTimeZoneMinutes == obj.fTimeZoneMinutes && fYear == obj.fYear && fMonth == obj.fMonth && fDayOfWeek == obj.fDayOfWeek && fDay == obj.fDay && fHour == obj.fHour && fMinute == obj.fMinute && fSecond == obj.fSecond;
-#pragma warning restore CS8909
-
-		public readonly override bool Equals (object obj) =>
-			obj is SKTimeDateTimeInternal f && Equals (f);
-
-		public static bool operator == (SKTimeDateTimeInternal left, SKTimeDateTimeInternal right) =>
-			left.Equals (right);
-
-		public static bool operator != (SKTimeDateTimeInternal left, SKTimeDateTimeInternal right) =>
-			!left.Equals (right);
-
-		public readonly override int GetHashCode ()
-		{
-			var hash = new HashCode ();
-			hash.Add (fTimeZoneMinutes);
-			hash.Add (fYear);
-			hash.Add (fMonth);
-			hash.Add (fDayOfWeek);
-			hash.Add (fDay);
-			hash.Add (fHour);
-			hash.Add (fMinute);
-			hash.Add (fSecond);
-			return hash.ToHashCode ();
-		}
-
-	}
-
 	// sk_webpencoder_options_t
 	[StructLayout (LayoutKind.Sequential)]
 	public readonly unsafe partial struct SKWebpEncoderOptions : IEquatable<SKWebpEncoderOptions> {
@@ -20281,30 +20281,32 @@ namespace SkiaSharp {
 		Bgr101010x = 10,
 		// BGR_101010X_XR_SK_COLORTYPE = 11
 		Bgr101010xXr = 11,
-		// GRAY_8_SK_COLORTYPE = 12
-		Gray8 = 12,
-		// RGBA_F16_NORM_SK_COLORTYPE = 13
-		RgbaF16Norm = 13,
-		// RGBA_F16_SK_COLORTYPE = 14
-		RgbaF16 = 14,
-		// RGBA_F32_SK_COLORTYPE = 15
-		RgbaF32 = 15,
-		// R8G8_UNORM_SK_COLORTYPE = 16
-		R8g8Unorm = 16,
-		// A16_FLOAT_SK_COLORTYPE = 17
-		A16Float = 17,
-		// R16G16_FLOAT_SK_COLORTYPE = 18
-		R16g16Float = 18,
-		// A16_UNORM_SK_COLORTYPE = 19
-		A16Unorm = 19,
-		// R16G16_UNORM_SK_COLORTYPE = 20
-		R16g16Unorm = 20,
-		// R16G16B16A16_UNORM_SK_COLORTYPE = 21
-		R16g16b16a16Unorm = 21,
-		// SRGBA_8888_SK_COLORTYPE = 22
-		Srgba8888 = 22,
-		// R8_UNORM_SK_COLORTYPE = 23
-		R8Unorm = 23,
+		// RGBA_10X6_SK_COLORTYPE = 12
+		Rgba10x6 = 12,
+		// GRAY_8_SK_COLORTYPE = 13
+		Gray8 = 13,
+		// RGBA_F16_NORM_SK_COLORTYPE = 14
+		RgbaF16Norm = 14,
+		// RGBA_F16_SK_COLORTYPE = 15
+		RgbaF16 = 15,
+		// RGBA_F32_SK_COLORTYPE = 16
+		RgbaF32 = 16,
+		// R8G8_UNORM_SK_COLORTYPE = 17
+		R8g8Unorm = 17,
+		// A16_FLOAT_SK_COLORTYPE = 18
+		A16Float = 18,
+		// R16G16_FLOAT_SK_COLORTYPE = 19
+		R16g16Float = 19,
+		// A16_UNORM_SK_COLORTYPE = 20
+		A16Unorm = 20,
+		// R16G16_UNORM_SK_COLORTYPE = 21
+		R16g16Unorm = 21,
+		// R16G16B16A16_UNORM_SK_COLORTYPE = 22
+		R16g16b16a16Unorm = 22,
+		// SRGBA_8888_SK_COLORTYPE = 23
+		Srgba8888 = 23,
+		// R8_UNORM_SK_COLORTYPE = 24
+		R8Unorm = 24,
 	}
 
 	// sk_encoded_image_format_t
