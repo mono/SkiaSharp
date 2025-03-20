f99e99de33a4523d7ed4728df0d6baafc0d10eae
Matthew Leibowitz <mattleibow@live.com>
2025-03-21 00:29:46 +0700

    Get the machines to do it for me

---
+++ scripts/get-artifact-names.ps1
@@ -0,0 +1,26 @@
+Param (
+    [Parameter(Mandatory, ValueFromPipeline)] [string] $ArtifactsJson
+)
+
+Write-Host "Using JSON:"
+Write-Host "============================================================"
+Write-Host $ArtifactsJson
+Write-Host "============================================================"
+Write-Host ""
+
+Write-Host "Looking for artifacts..."
+$json = ConvertFrom-Json $ArtifactsJson
+$objects = $json | Get-Member -MemberType NoteProperty
+$names = $objects | ForEach-Object { $json."$($_.Name)".name }
+
+Write-Host "Found $($names.Count) item[s]:"
+$names | ForEach-Object { Write-Host " - $_" }
+
+$actualNames = $names | Where-Object { $json."$_".result -ne "Skipped" }
+$skippedNames = $names | Where-Object { $actualNames -notcontains $_ }
+
+Write-Host "Final $($skippedNames.Count) item[s]:"
+$actualNames | ForEach-Object { Write-Host " - $_" }
+
+Write-Host "Skipping $($skippedNames.Count) item[s]:"
+$skippedNames | ForEach-Object { Write-Host " - $_" }
