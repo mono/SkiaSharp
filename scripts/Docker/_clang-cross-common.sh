018d6b4c3ced26e0f7781c4eb28abb0a8dcce1cb
東雲なの <87938880+4Darmygeometry@users.noreply.github.com>
2025-03-24 02:23:40 +0800

    Update _clang-cross-common.sh

--- scripts/Docker/_clang-cross-common.sh
+++ scripts/Docker/_clang-cross-common.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env bash
 set -ex
 
+# Parameters:
+# $1 - The directory containing the Dockerfile  [ clang-cross/10 | clang-cross ]
+# $2 - The target architecture to build for     [ arm | arm64 | riscv64 | x86 | x64 | loongarch64 ]
+# $3 - The ABI                                  [ gnu | musl ]
+# $4 - The variant                              [ "" | alpine ]
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# the directory containing the Dockerfile
 DOCKER_DIR="$1"
 
 # the target architecture to build for
-ARCH="${2:-arm}"
+ARCH="$2"
 
 # the docker image architecture to use
 MACHINE_ARCH="$(uname -m)"
-[ "$MACHINE_ARCH" = "arm64" ] && MACHINE_ARCH=aarch64
-IMAGE_ARCH="${3:-$([[ "$MACHINE_ARCH" == "aarch64" ]] && echo "arm64v8" || echo "amd64")}"
-
-DISTRO_VERSION=$4
-ABI=$5
-VENDOR=$6
-
-case $ARCH in
-  arm)           TOOLCHAIN_ARCH=arm$VENDOR-linux-${ABI}eabihf     ; TOOLCHAIN_ARCH_SHORT=armhf     ; TARGET_MACHINE_ARCH=armhf       ;;
-  arm64)         TOOLCHAIN_ARCH=aarch64$VENDOR-linux-$ABI         ; TOOLCHAIN_ARCH_SHORT=arm64     ; TARGET_MACHINE_ARCH=aarch64     ;;
-  loongarch64)   TOOLCHAIN_ARCH=loongarch64$VENDOR-linux-$ABI     ; TOOLCHAIN_ARCH_SHORT=loong64   ; TARGET_MACHINE_ARCH=loongarch64 ;;
-  riscv64)       TOOLCHAIN_ARCH=riscv64$VENDOR-linux-$ABI         ; TOOLCHAIN_ARCH_SHORT=riscv64   ; TARGET_MACHINE_ARCH=riscv64     ;;
-  x86)           TOOLCHAIN_ARCH=i686$VENDOR-linux-$ABI            ; TOOLCHAIN_ARCH_SHORT=i386      ; TARGET_MACHINE_ARCH=x86         ;;
-  x64)           TOOLCHAIN_ARCH=x86-64$VENDOR-linux-$ABI          ; TOOLCHAIN_ARCH_SHORT=amd64     ; TARGET_MACHINE_ARCH=x86_64      ;;
-  *) echo "Unsupported architecture: $ARCH" && exit 1 ;;
+case $MACHINE_ARCH in
+  arm64) IMAGE_ARCH=arm64v8 ; MACHINE_ARCH=aarch64 ;;
+  *)     IMAGE_ARCH=amd64   ;;
 esac
 
-(cd $DIR && docker build --tag skiasharp-linux-$ABI-cross-$ARCH \
-    --build-arg TOOLCHAIN_ARCH=$TOOLCHAIN_ARCH                  \
-    --build-arg TOOLCHAIN_ARCH_SHORT=$TOOLCHAIN_ARCH_SHORT      \
-    --build-arg IMAGE_ARCH=$IMAGE_ARCH                          \
-    --build-arg MACHINE_ARCH=$MACHINE_ARCH                      \
-    --build-arg TARGET_MACHINE_ARCH=$TARGET_MACHINE_ARCH        \
-    --build-arg DISTRO_VERSION=$DISTRO_VERSION                  \
+# the ABI
+ABI=$3
+
+# the variant
+VARIANT=$4
+
+(cd $DIR && 
+  docker build --tag skiasharp-linux-$ABI-cross-$ARCH \
+    --build-arg BUILD_ARCH=$ARCH                      \
+    --build-arg IMAGE_ARCH=$IMAGE_ARCH                \
+    --build-arg MACHINE_ARCH=$MACHINE_ARCH            \
     $DOCKER_DIR)
 
-if [ "$VENDOR" = "-alpine" ]; then vendor=alpine; fi
+[ -n "$VARIANT" ] && VARIANT="--variant=$VARIANT"
 
 (cd $DIR/../.. && 
-    docker run --rm --name skiasharp-linux-$ABI-cross-$ARCH --volume $(pwd):/work skiasharp-linux-$ABI-cross-$ARCH /bin/bash -c "\
-        dotnet tool restore &&
-        dotnet cake --target=externals-linux-clang-cross --configuration=Release --buildarch=$ARCH --variant=$vendor")
+    docker run --rm --name skiasharp-linux-$ABI-cross-$ARCH --volume $(pwd):/work skiasharp-linux-$ABI-cross-$ARCH /bin/bash -c " \
+        dotnet tool restore ; \
+        dotnet cake --target=externals-linux-clang-cross --configuration=Release --buildarch=$ARCH $VARIANT ")
