7812abde492969e88cb906450544f2e069135e3e
東雲なの <87938880+4Darmygeometry@users.noreply.github.com>
2025-03-24 02:36:18 +0800

    Update build-local.sh

--- scripts/Docker/debian/clang-cross/build-local.sh
+++ scripts/Docker/debian/clang-cross/build-local.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
+# Parameters:
+# $1 - The target architecture to build for     [ arm | arm64 | riscv64 | x86 | x64 | loongarch64 ]
+# $2 - The Debian distro version                [ 10 | 12 | 13(Loongnix 25) ]
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# the target architecture to build for
-ARCH=$1
-DEBIAN_VERSION=$3
-if [ -z "$DEBIAN_VERSION" ]; then
-  case $ARCH in
-    loongarch64) DEBIAN_VERSION=13 ;;
-    riscv64) DEBIAN_VERSION=12     ;;
-    *) DEBIAN_VERSION=11           ;;
-  esac
-fi
+ARCH="${1:-arm}"
+DEBIAN_VERSION="${2:-10}"
 
-$DIR/../../_clang-cross-common.sh "$DIR/$DEBIAN_VERSION" "$ARCH" "$2" "$DEBIAN_VERSION" "gnu"
+$DIR/../../_clang-cross-common.sh "$DIR/$DEBIAN_VERSION" "$ARCH" "gnu"
