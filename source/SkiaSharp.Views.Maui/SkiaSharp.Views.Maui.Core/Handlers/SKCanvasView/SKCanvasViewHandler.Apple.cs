25a4490b78b95653188d5fd91446aa44bf156b9a
Nick Kovalsky <taublast@gmail.com>
2025-06-17 12:07:56 +0300

    add safety checks to mappers

--- source/SkiaSharp.Views.Maui/SkiaSharp.Views.Maui.Core/Handlers/SKCanvasView/SKCanvasViewHandler.Apple.cs
+++ source/SkiaSharp.Views.Maui/SkiaSharp.Views.Maui.Core/Handlers/SKCanvasView/SKCanvasViewHandler.Apple.cs
@@ -36,16 +36,25 @@ namespace SkiaSharp.Views.Maui.Handlers
 
 		public static void OnInvalidateSurface(SKCanvasViewHandler handler, ISKCanvasView canvasView, object? args)
 		{
+			if (handler?.PlatformView == null)
+				return;
+
 			handler.PlatformView.SetNeedsDisplay();
 		}
 
 		public static void MapIgnorePixelScaling(SKCanvasViewHandler handler, ISKCanvasView canvasView)
 		{
+			if (handler?.PlatformView == null)
+				return;
+
 			handler.PlatformView.IgnorePixelScaling = canvasView.IgnorePixelScaling;
 		}
 
 		public static void MapEnableTouchEvents(SKCanvasViewHandler handler, ISKCanvasView canvasView)
 		{
+			if (handler?.PlatformView == null)
+				return;
+
 			handler.touchProxy?.UpdateEnableTouchEvents(handler.PlatformView, canvasView.EnableTouchEvents);
 		}
 
