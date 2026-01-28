74cfb7fce162850bf9490d1730cc32ecd5b5826c
Matthew Leibowitz <mattleibow@live.com>
2026-01-28 23:09:49 +0200

    Remove redundant TapGestureRecognizer from menu navigation
    CollectionView's built-in SelectionChanged event already handles navigation.
    Removed:
    - TapGestureRecognizer from MenuItemTemplate in Styles.xaml
    - NavigateCommand and constructor from HomeBasePage.cs
    - System.Windows.Input using directive
    
    Warnings reduced from 262 to 181.

--- samples/DocsSamplesApp/DocsSamplesApp/HomeBasePage.cs
+++ samples/DocsSamplesApp/DocsSamplesApp/HomeBasePage.cs
@@ -1,24 +1,7 @@
-﻿using System.Windows.Input;
-
-namespace DocsSamplesApp;
+﻿namespace DocsSamplesApp;
 
 public class HomeBasePage : ContentPage
 {
-    public HomeBasePage()
-    {
-        NavigateCommand = new Command<Type?>(async (Type? pageType) =>
-        {
-            if (pageType is null) return;
-            var page = (Page?)Activator.CreateInstance(pageType);
-            if (page is not null)
-                await Navigation.PushAsync(page);
-        });
-
-        BindingContext = this;
-    }
-
-    public ICommand NavigateCommand { get; }
-
     protected async void OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
         if (e.CurrentSelection.FirstOrDefault() is MenuItem item && item.PageType is not null)
