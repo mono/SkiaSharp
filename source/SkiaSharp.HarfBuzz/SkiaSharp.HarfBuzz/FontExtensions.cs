4c4f2c4f0ff947374fc9dd29afc1c162ac3b4554
ScrubN <72096833+ScrubN@users.noreply.github.com>
2026-05-09 11:42:51 -0400

    Add FontExtensions.GetShapedTextPath extensions

--- source/SkiaSharp.HarfBuzz/SkiaSharp.HarfBuzz/FontExtensions.cs
+++ source/SkiaSharp.HarfBuzz/SkiaSharp.HarfBuzz/FontExtensions.cs
@@ -26,5 +26,76 @@ namespace SkiaSharp.HarfBuzz
 
 			font.SetScale(scale.Width, scale.Height);
 		}
+
+		public static SKPath GetShapedTextPath(this SKFont font, string text, SKPoint p) =>
+			font.GetShapedTextPath(text, p.X, p.Y);
+
+		public static SKPath GetShapedTextPath(this SKFont font, string text, float x, float y) =>
+			font.GetShapedTextPath(text, x, y, SKTextAlign.Left);
+
+		public static SKPath GetShapedTextPath(this SKFont font, string text, float x, float y, SKTextAlign textAlign)
+		{
+			if (string.IsNullOrEmpty(text))
+				return new SKPath();
+
+			using var shaper = new SKShaper(font.Typeface);
+			return font.GetShapedTextPath(shaper, text, x, y, textAlign);
+		}
+
+		public static SKPath GetShapedTextPath(this SKFont font, SKShaper shaper, string text, float x, float y, SKTextAlign textAlign)
+		{
+			if (string.IsNullOrEmpty(text))
+				return new SKPath();
+
+			if (shaper == null)
+				throw new ArgumentNullException(nameof(shaper));
+			if (font == null)
+				throw new ArgumentNullException(nameof(font));
+
+			font.Typeface = shaper.Typeface;
+
+			// shape the text
+			var result = shaper.Shape(text, x, y, font);
+
+			// adjust alignment
+			var xOffset = 0.0f;
+			if (textAlign != SKTextAlign.Left)
+			{
+				var width = result.Width;
+				if (textAlign == SKTextAlign.Center)
+					width *= 0.5f;
+				xOffset -= width;
+			}
+
+			// get spans for more efficient accessing
+			var glyphSpan = result.Codepoints.AsSpan();
+			var pointSpan = result.Points.AsSpan();
+
+			using var pathBuilder = new SKPathBuilder();
+
+			// generate a path for each glyph
+			for (var i = 0; i < pointSpan.Length; i++)
+			{
+				// get the glyph path
+				using var glyphPath = font.GetGlyphPath((ushort)glyphSpan[i]);
+
+				if (glyphPath is null || glyphPath.IsEmpty)
+					continue;
+
+				// translate the glyph path
+				var point = pointSpan[i];
+				var matrix = new SKMatrix(
+					1, 0, point.X + xOffset,
+					0, 1, point.Y,
+					0, 0, 1
+				);
+				glyphPath.Transform(in matrix);
+
+				// append the glyph path
+				pathBuilder.AddPath(glyphPath);
+			}
+
+			return pathBuilder.Detach();
+		}
 	}
 }
